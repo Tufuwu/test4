9befd8ea1c0d30d292e7122d00380f87d80c2658
wu <wu@.com>
2025-04-06 20:28:05 +0800

    ucla-irl/ndn-python-repo/action

--- docs/conf.py
+++ docs/conf.py
@@ -1,69 +1,61 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2024 Jan Holthuis <jan.holthuis@rub.de>
+# Configuration file for the Sphinx documentation builder.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# SPDX-License-Identifier: BSD-2-Clause
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-"""Sphinx configuration file."""
+# -- Path setup --------------------------------------------------------------
 
-import time
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+import sphinx_rtd_theme
 
-author = "Jan Holthuis"
-project = "sphinx-multiversion"
-release = "0.2.5"
-version = "0.2"
-copyright = "{}, {}".format(time.strftime("%Y"), author)
 
-html_theme = "alabaster"
-html_theme_options = {
-    "github_repo": "multiversion",
-    "github_user": "sphinx-contrib",
-    "github_banner": True,
-    "github_button": True,
-    "show_relbar_bottom": True,
-}
-html_last_updated_fmt = "%c"
-master_doc = "index"
-pygments_style = "friendly"
-templates_path = ["_templates"]
+# -- Project information -----------------------------------------------------
+
+project = 'ndn-python-repo'
+copyright = '2020, Zhaoning Kong'
+author = 'Zhaoning Kong'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    "sphinx_multiversion",
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
+    'sphinx_autodoc_typehints',
 ]
 
-templates_path = [
-    "_templates",
-]
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
 
-html_sidebars = {
-    "**": [
-        "about.html",
-        "navigation.html",
-        "relations.html",
-        "searchbox.html",
-        "versioning.html",
-    ],
-}
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-smv_remote_whitelist = r"^origin$"
-smv_branch_whitelist = r"^main$"
+# The master toctree document.
+master_doc = 'index'
+
+autoclass_content = 'both'
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+pygments_style = 'sphinx'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,28 +1,29 @@
-=============================
-sphinx-multiversion |release|
-=============================
+.. ndn-python-repo documentation master file, created by
+   sphinx-quickstart on Tue May 12 16:51:55 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
 
-A Sphinx extension for building self-hosted versioned documentation.
-
-Project Links
-=============
-
-* Source code: https://github.com/sphinx-contrib/multiversion
+Welcome to ndn-python-repo's documentation!
+===========================================
 
 .. toctree::
-    :maxdepth: 3
-    :caption: General
+   :maxdepth: 2
+   :caption: Contents:
 
-    install
-    quickstart
-    configuration
-    templates
-    context
+   src/readme.rst
+   src/install.rst
+   src/configuration.rst
+   src/specification/specification.rst
+   src/misc_pkgs/misc_pkgs.rst
+   src/examples.rst
+   src/authors.rst
+   src/future.rst
 
-.. toctree::
-    :maxdepth: 1
-    :caption: Appendix
 
-    github_pages
-    faq
-    changelog
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
