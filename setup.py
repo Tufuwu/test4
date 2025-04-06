86139c7feda5f9dcc2c70869fc9d7c9b60dbb260
wu <wu@.com>
2025-04-06 23:09:26 +0800

    espressif/esptool/action

--- setup.py
+++ setup.py
@@ -1,154 +1,133 @@
-# coding: utf-8
-#
-# Copyright 2011 Yesudeep Mangalapilly <yesudeep@gmail.com>
-# Copyright 2012 Google, Inc & contributors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from __future__ import division, print_function
 
-import importlib.util
-import sys
+import io
 import os
-import os.path
-from platform import machine
-from setuptools import setup, find_packages
-from setuptools.extension import Extension
-from setuptools.command.build_ext import build_ext
+import re
+import sys
 
-SRC_DIR = 'src'
-WATCHDOG_PKG_DIR = os.path.join(SRC_DIR, 'watchdog')
+try:
+    from setuptools import find_packages, setup
+except ImportError:
+    print('Package setuptools is missing from your Python installation. Please see the installation section of '
+          'the README for instructions on how to install it.')
+    exit(1)
 
-# Load the module version
-spec = importlib.util.spec_from_file_location(
-    'version', os.path.join(WATCHDOG_PKG_DIR, 'version.py'))
-version = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(version)
 
-# Ignored Apple devices on which compiling watchdog_fsevents.c would fail.
-# The FORCE_MACOS_MACHINE envar, when set to 1, will force the compilation.
-_apple_devices = ('appletv', 'iphone', 'ipod', 'ipad', 'watch')
-is_macos = sys.platform == 'darwin' and not machine().lower().startswith(_apple_devices)
+# Example code to pull version from esptool.py with regex, taken from
+# http://python-packaging-user-guide.readthedocs.org/en/latest/single_source_version/
+def read(*names, **kwargs):
+    with io.open(
+            os.path.join(os.path.dirname(__file__), *names),
+            encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
 
-ext_modules = []
-if is_macos or os.getenv('FORCE_MACOS_MACHINE', '0') == '1':
-    ext_modules = [
-        Extension(
-            name='_watchdog_fsevents',
-            sources=[
-                'src/watchdog_fsevents.c',
-            ],
-            libraries=['m'],
-            define_macros=[
-                ('WATCHDOG_VERSION_STRING',
-                 '"' + version.VERSION_STRING + '"'),
-                ('WATCHDOG_VERSION_MAJOR', version.VERSION_MAJOR),
-                ('WATCHDOG_VERSION_MINOR', version.VERSION_MINOR),
-                ('WATCHDOG_VERSION_BUILD', version.VERSION_BUILD),
-            ],
-            extra_link_args=[
-                '-framework', 'CoreFoundation',
-                '-framework', 'CoreServices',
-            ],
-            extra_compile_args=[
-                '-std=c99',
-                '-pedantic',
-                '-Wall',
-                '-Wextra',
-                '-fPIC',
 
-                # Issue #620
-                '-Wno-nullability-completeness',
-                # Issue #628
-                '-Wno-nullability-extension',
-                '-Wno-newline-eof',
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
-                # required w/Xcode 5.1+ and above because of '-mno-fused-madd'
-                '-Wno-error=unused-command-line-argument'
-            ]
-        ),
-    ]
 
-extras_require = {
-    'watchmedo': ['PyYAML>=3.10', 'argh>=0.24.1'],
-}
+long_description = """
+==========
+esptool.py
+==========
+A command line utility to communicate with the ROM bootloader in Espressif ESP8266 & ESP32 microcontrollers.
 
-with open('README.rst', encoding='utf-8') as f:
-    readme = f.read()
+Allows flashing firmware, reading back firmware, querying chip parameters, etc.
 
-with open('changelog.rst', encoding='utf-8') as f:
-    changelog = f.read()
+The esptool.py project is hosted on github: https://github.com/espressif/esptool
 
-setup(name="watchdog",
-      version=version.VERSION_STRING,
-      description="Filesystem events monitoring",
-      long_description=readme + '\n\n' + changelog,
-      author="Yesudeep Mangalapilly",
-      author_email="yesudeep@gmail.com",
-      license="Apache License 2.0",
-      url="http://github.com/gorakhargosh/watchdog",
-      keywords=' '.join([
-          'python',
-          'filesystem',
-          'monitoring',
-          'monitor',
-          'FSEvents',
-          'kqueue',
-          'inotify',
-          'ReadDirectoryChangesW',
-          'polling',
-          'DirectorySnapshot',
-      ]),
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: Apache Software License',
-          'Natural Language :: English',
-          'Operating System :: POSIX :: Linux',
-          'Operating System :: MacOS :: MacOS X',
-          'Operating System :: POSIX :: BSD',
-          'Operating System :: Microsoft :: Windows :: Windows Vista',
-          'Operating System :: Microsoft :: Windows :: Windows 7',
-          'Operating System :: Microsoft :: Windows :: Windows 8',
-          'Operating System :: Microsoft :: Windows :: Windows 8.1',
-          'Operating System :: Microsoft :: Windows :: Windows 10',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3 :: Only',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Programming Language :: C',
-          'Topic :: Software Development :: Libraries',
-          'Topic :: System :: Monitoring',
-          'Topic :: System :: Filesystems',
-          'Topic :: Utilities',
-      ],
-      package_dir={'': SRC_DIR},
-      packages=find_packages(SRC_DIR),
-      include_package_data=True,
-      extras_require=extras_require,
-      cmdclass={
-          'build_ext': build_ext,
-      },
-      ext_modules=ext_modules,
-      entry_points={'console_scripts': [
-          'watchmedo = watchdog.watchmedo:main [watchmedo]',
-      ]},
-      python_requires='>=3.6',
-      zip_safe=False
+Installation
+------------
+
+esptool can be installed via pip:
+
+  $ pip install --upgrade esptool
+
+Since version 1.3, esptool supports both Python 2.7 and Python 3.4 or newer.
+
+Since version 2.0, esptool supports both ESP8266 & ESP32.
+
+Usage
+-----
+
+Please see the `Usage section of the README.md file <https://github.com/espressif/esptool#usage>`_.
+
+You can also get help information by running `esptool.py --help`.
+
+Contributing
+------------
+Please see the `CONTRIBUTING.md file on github <https://github.com/espressif/esptool/blob/master/CONTRIBUTING.md>`_.
+"""
+
+# For Windows, we want to install esptool.py.exe, etc. so that normal Windows command line can run them
+# For Linux/macOS, we can't use console_scripts with extension .py as their names will clash with the modules' names.
+if os.name == "nt":
+    scripts = None
+    entry_points = {
+        'console_scripts': [
+            'esptool.py=esptool:_main',
+            'espsecure.py=espsecure:_main',
+            'espefuse.py=espefuse:_main',
+        ],
+    }
+else:
+    scripts = ['esptool.py',
+               'espsecure.py',
+               'espefuse.py']
+    entry_points = None
+
+setup(
+    name='esptool',
+    py_modules=['esptool', 'espsecure', 'espefuse'],
+    version=find_version('esptool.py'),
+    description='A serial utility to communicate & flash code to Espressif ESP8266 & ESP32 chips.',
+    long_description=long_description,
+    url='https://github.com/espressif/esptool',
+    author='Fredrik Ahlberg (themadinventor) & Angus Gratton (projectgus)',
+    author_email='angus@espressif.com',
+    license='GPLv2+',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Embedded Systems',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',  # Note: when dropping 3.4 support we can also remove the check in setup_requires
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    setup_requires=['wheel'] if sys.version_info[0:2] not in [(3, 4), (3, 5)] else [],
+    extras_require={
+        "dev": [
+            'flake8>=3.2.0',
+            'flake8-future-import',
+            'flake8-import-order',
+            'pyelftools',
+            'unittest-xml-reporting<=2.5.2',  # the replacement of the old xmlrunner package (Python 2 comp. version)
+            'coverage',
+        ],
+    },
+    install_requires=[
+        'bitstring>=3.1.6',
+        'cryptography>=2.1.4',
+        'ecdsa>=0.16.0',
+        'pyserial>=3.0',
+        'reedsolo>=1.5.3,<=1.5.4',
+    ],
+    packages=find_packages(),
+    scripts=scripts,
+    entry_points=entry_points,
 )
