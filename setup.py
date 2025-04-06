d326e2a9d7dba4fd207e92f8f392b9c26f4e5f73
wu <wu@.com>
2025-04-07 02:18:38 +0800

    srittau/python-asserts/action

--- setup.py
+++ setup.py
@@ -1,59 +1,36 @@
-# Copyright (c) 2016-2020 Renata Hodovan, Akos Kiss.
-#
-# Licensed under the BSD 3-Clause License
-# <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
-# This file may not be copied, modified, or distributed except
-# according to those terms.
+#!/usr/bin/env python
 
-from setuptools import find_packages, setup
+import os
+from setuptools import setup
 
 
-def picire_version():
-    def _version_scheme(version):
-        return version.format_with('{tag}')
-
-    def _local_scheme(version):
-        if version.exact and not version.dirty:
-            return ''
-        parts = ['{distance}'.format(distance=version.distance)]
-        if version.node:
-            parts.append('{node}'.format(node=version.node))
-        if version.dirty:
-            parts.append('d{time:%Y%m%d}'.format(time=version.time))
-        return '+{parts}'.format(parts='.'.join(parts))
-
-    return { 'version_scheme': _version_scheme, 'local_scheme': _local_scheme }
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
-    name='picire',
-    packages=find_packages(),
-    url='https://github.com/renatahodovan/picire',
-    license='BSD',
-    author='Renata Hodovan, Akos Kiss',
-    author_email='hodovan@inf.u-szeged.hu, akiss@inf.u-szeged.hu',
-    description='Picire Parallel Delta Debugging Framework',
-    long_description=open('README.rst').read(),
-    install_requires=['chardet', 'psutil', 'setuptools'],
-    zip_safe=False,
-    include_package_data=True,
-    setup_requires=['setuptools_scm'],
-    use_scm_version=picire_version,
-    entry_points={
-        'console_scripts': ['picire = picire.cli:execute']
-    },
+    name="asserts",
+    version="0.11.0",
+    description="Stand-alone Assertions",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Sebastian Rittau",
+    author_email="srittau@rittau.biz",
+    url="https://github.com/srittau/python-asserts",
+    packages=["asserts"],
+    package_data={"asserts": ["py.typed", "*.pyi"]},
+    python_requires=">=3.6",
+    license="MIT",
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Software Development :: Testing',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
     ],
 )
