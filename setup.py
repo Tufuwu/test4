633728df35fa0ca7d15e3f2a3847f6662a4b6183
wu <wu@.com>
2025-04-06 21:54:53 +0800

    eerimoq/cantools/action

--- setup.py
+++ setup.py
@@ -1,40 +1,39 @@
+#!/usr/bin/env python
+
 from setuptools import setup
+from setuptools import find_packages
+import re
 
-setup(
-    name='homu',
-    version='0.3.0',
-    author='Barosl Lee',
-    url='https://github.com/barosl/homu',
-    test_suite='homu.tests',
-    description=('A bot that integrates with GitHub '
-                 'and your favorite continuous integration service'),
 
-    packages=['homu'],
-    install_requires=[
-        'github3.py==0.9.6',
-        'toml',
-        'Jinja2',
-        'requests',
-        'bottle',
-        'waitress',
-        'retrying',
-    ],
-    setup_requires=[
-        'pytest-runner',
-    ],
-    tests_require=[
-        'pytest',
-    ],
-    package_data={
-        'homu': [
-            'html/*.html',
-            'assets/*',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-            'homu=homu.main:main',
-        ],
-    },
-    zip_safe=False,
-)
+def find_version():
+    return re.search(r"^__version__ = '(.*)'$",
+                     open('cantools/version.py', 'r').read(),
+                     re.MULTILINE).group(1)
+
+
+setup(name='cantools',
+      version=find_version(),
+      description='CAN BUS tools.',
+      long_description=open('README.rst', 'r').read(),
+      author='Erik Moqvist',
+      author_email='erik.moqvist@gmail.com',
+      license='MIT',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ],
+      keywords=['can', 'can bus', 'dbc', 'kcd', 'automotive'],
+      url='https://github.com/eerimoq/cantools',
+      packages=find_packages(exclude=['tests']),
+      install_requires=[
+          'bitstruct>=6.0.0',
+          'python-can>=2.2.0',
+          'textparser>=0.21.1',
+          'diskcache',
+          'argparse_addons'
+      ],
+      test_suite="tests",
+      entry_points = {
+          'console_scripts': ['cantools=cantools.__init__:_main']
+      })
