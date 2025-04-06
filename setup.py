91cedc50912f1f344bbeab3e0b75bc60241ae426
wu <wu@.com>
2025-04-07 00:30:27 +0800

    benkonrath/django-csv-export-view/action

--- setup.py
+++ setup.py
@@ -1,40 +1,71 @@
 #!/usr/bin/env python
-
 import os
+import re
+import sys
+from codecs import open
+from distutils.core import setup
+from os import path
 
-from setuptools import setup, find_packages
+from setuptools import find_packages
 
 
-long_description = ""
-if os.path.isfile("README.rst"):
-    long_description = open("README.rst", "r").read()
+def get_long_description():
+    current_dir = path.abspath(path.dirname(__file__))
+    readme_path = path.join(current_dir, "README.md")
+    with open(readme_path, encoding="utf-8") as f:
+        return f.read()
+
+
+def get_version():
+    version_file = open(path.join("csv_export", "__init__.py"), encoding="utf-8").read()
+    version_match = re.search(
+        r"__version__ = ['\"]([0-9]+\.[0-9]+\.[0-9]+(\.dev[0-9])?)['\"]", version_file, re.MULTILINE
+    )
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")  # noqa
+    print("You should also add a git tag for this version:")
+    print(" git tag {}".format(get_version()))
+    print(" git push --tags")
+    sys.exit()
 
 
 setup(
-    name="img2gb",
-    version="1.1.0",
-    description="Converts images to GameBoy tileset",
-    url="",
-    license="BSD-3-Clause",
-    long_description=long_description,
-    keywords="gb gameboy image tile tileset tilemap",
-    author="Fabien LOISON",
-    packages=find_packages(),
-    setup_requires=["cffi>=1.0.0"],
+    name="django-csv-export-view",
+    version=get_version(),
+    license="BSD",
+    description="Django class-based view for CSV exports",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/benkonrath/django-csv-export-view",
+    author="Ben Konrath",
+    author_email="ben@bagu.org",
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    data_files=[("", ["README.md", "CHANGELOG.md"])],
+    zip_safe=False,
     install_requires=[
-        "pillow>=5.0.0",
+        "django>=3.2",
     ],
-    extras_require={
-        "dev": [
-            "nox",
-            "flake8",
-            "pytest",
-            "black",
-        ]
-    },
-    entry_points={
-        "console_scripts": [
-            "img2gb = img2gb.__main__:main",
-        ],
-    },
+    python_requires="!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Utilities",
+    ],
 )
