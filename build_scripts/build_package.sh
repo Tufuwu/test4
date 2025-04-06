586d7179e9ec1d6b1355f6409e5249a71fb1d87a
wu <wu@.com>
2025-04-06 23:15:35 +0800

    dirkmoors/drf-tus/action

--- build_scripts/build_package.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Building package"
-rm -rf dist/
-export BOTO_CONFIG=/dev/null
-python setup.py sdist bdist_wheel
-ls -l dist/
--- setup.py
+++ setup.py
@@ -1,62 +1,96 @@
-# Copyright 2019 Adobe. All rights reserved.
-# This file is licensed to you under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License. You may obtain a copy
-# of the License at http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software distributed under
-# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-# OF ANY KIND, either express or implied. See the License for the specific language
-# governing permissions and limitations under the License.
-
+#!/usr/bin/env python
 import os
+import re
 import sys
 
 try:
-    from setuptools import setup, find_packages
+    from setuptools import setup
 except ImportError:
-    from distutils.core import setup, find_packages
+    from distutils.core import setup
 
-with open('README.md') as f:
-    _readme = f.read()
 
-_mydir = os.path.abspath(os.path.dirname(sys.argv[0]))
-_requires = [r for r in open(os.path.sep.join((_mydir, 'requirements.txt')), "r").read().split('\n') if len(r) > 1]
+def get_version(*file_paths):
+    """Retrieves the version from rest_framework_tus/__init__.py"""
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]",
+        version_file,
+        re.M,
+    )
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+version = get_version("rest_framework_tus", "__init__.py")
+
+
+if sys.argv[-1] == "publish":
+    try:
+        import wheel
+
+        print("Wheel version: ", wheel.__version__)
+    except ImportError:
+        print('Wheel library missing. Please run "pip install wheel"')
+        sys.exit()
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
+    sys.exit()
+
+if sys.argv[-1] == "tag":
+    print("Tagging the version on git:")
+    os.system(f"git tag -a {version} -m 'version {version}'")
+    os.system("git push --tags")
+    sys.exit()
+
+readme = open("README.rst").read()
+history = open("HISTORY.rst").read().replace(".. :changelog:", "")
+
 setup(
-    name='ops-cli',
-    version='2.0.6',
-    description='Ops - wrapper for Terraform, Ansible, and SSH for cloud automation',
-    long_description=_readme + '\n\n',
-    long_description_content_type='text/markdown',
-    url='https://github.com/adobe/ops-cli',
-    python_requires='>=3.5',
-    author='Adobe',
-    author_email='noreply@adobe.com',
-    license='Apache2',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Text Processing :: Markup :: HTML'
+    name="drf-tus",
+    version=version,
+    description="A Tus (tus.io) library for Django Rest Framework",
+    long_description=f"{readme}\n\n{history}",
+    author="Dirk Moors",
+    author_email="dirkmoors@gmail.com",
+    url="https://github.com/dirkmoors/drf-tus",
+    packages=[
+        "rest_framework_tus",
     ],
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    package_data={
-        '': ['data/ansible/*', 'data/ansible/tasks/*', 'data/ssh/*', 'data/terraform/*']
-    },
-    install_requires=_requires,
-    entry_points={
-        'console_scripts': [
-            'ops = ops.main:run'
-        ]
-    }
+    include_package_data=True,
+    install_requires=[
+        "python-dateutil>=2.8.2",
+        "Django>=3.2,<=4.2",
+        "djangorestframework>=3.14.0",
+        "jsonfield>=2.0.0",
+        "django-fsm==2.8.1",
+        "six>=1.11.0",
+    ],
+    license="MIT",
+    zip_safe=False,
+    keywords=[
+        "drf-tus",
+        "tus",
+        "django",
+        "rest",
+        "framework",
+        "django-rest-framework",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
