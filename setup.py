af3ca21475b4c18746f5d123091352c25b6487df
wu <wu@.com>
2025-04-07 01:04:19 +0800

    pygame/pygameweb/action

--- setup.py
+++ setup.py
@@ -1,121 +1,89 @@
-from setuptools import setup, Command
-from setuptools.command.easy_install import ScriptWriter
-from setuptools.command.install import install as orig_install
-from distutils.command.build import build as orig_build
+""" pygameweb
+"""
+import io
+import os
+import re
+from itertools import chain
 
-from textwrap import dedent
-from subprocess import call
-import logging
-from os import path
-from glob import glob
+from setuptools import setup, find_packages
 
 
-comp_files = glob('completions/zsh/_*')
-icon_files = glob('icons/scalable/actions/udiskie-*.svg')
-languages  = [path.splitext(path.split(po_file)[1])[0]
-              for po_file in glob('lang/*.po')]
-
-
-class build(orig_build):
-    """Subclass build command to add a subcommand for building .mo files."""
-    sub_commands = orig_build.sub_commands + [('build_mo', None)]
-
-
-class build_mo(Command):
-
-    """Create machine specific translation files (for i18n via gettext)."""
-
-    description = 'Compile .po files into .mo files'
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        for lang in languages:
-            po_file = 'lang/{}.po'.format(lang)
-            mo_file = 'build/locale/{}/LC_MESSAGES/udiskie.mo'.format(lang)
-            self.mkpath(path.dirname(mo_file))
-            self.make_file(
-                po_file, mo_file, self.make_mo,
-                [po_file, mo_file])
-
-    def make_mo(self, po_filename, mo_filename):
-        """Create a machine object (.mo) from a portable object (.po) file."""
-        try:
-            call(['msgfmt', po_filename, '-o', mo_filename])
-        except OSError as e:
-            # ignore failures since i18n support is optional:
-            logging.warning(e)
-
-
-# NOTE: Subclassing the setuptools install command alters its behaviour to use
-# the distutils code. This is due to some really odd call-context checks in
-# the setuptools command.
-#
-# In fact this is desirable because distutils (correctly) installs data files
-# to `sys.prefix` whereas setuptools by default installs to the egg folder
-# (which is pretty much useless) and doesn't invoke build commands before
-# install. The only real drawback with the distutils behaviour is that it does
-# not automatically install dependencies, but we can easily live with that.
-#
-# Note further that we need to subclass the *setuptools* install command
-# rather than the *distutils* one to prevent errors when installing with pip
-# from the source distribution.
-class install(orig_install):
-
-    """Custom install command used to update the gtk icon cache."""
-
-    def run(self):
-        """Perform distutils-style install, then update GTK icon cache."""
-        orig_install.run(self)
-        try:
-            call(['gtk-update-icon-cache', 'share/icons/hicolor'])
-        except OSError as e:
-            # ignore failures since the tray icon is an optional component:
-            logging.warning(e)
-
-
-def fast_entrypoint_script_template():
+def read(*parts):
+    """ Reads in file from *parts.
     """
-    Replacement for ``easy_install.ScriptWriter.template`` to generate faster
-    entry points that don't depend on and import pkg_resources.
+    try:
+        return io.open(os.path.join(*parts), 'r', encoding='utf-8').read()
+    except IOError:
+        return ''
 
-    NOTE: `pip install` already does the right thing (at least for pip 19.0)
-    without our help, but this is still needed for setuptools install, i.e.
-    ``python setup.py install`` or develop.
+
+def get_version():
+    """ Returns version from pygameweb/__init__.py
     """
-    SCRIPT_TEMPLATE = dedent(r'''
-        # encoding: utf-8
-        import sys
-        from {ep.module_name} import {ep.attrs[0]}
-
-        if __name__ == '__main__':
-            sys.exit({func}())
-    ''').lstrip()
-
-    class ScriptTemplate(str):
-        def __mod__(self, context):
-            func = '.'.join(context['ep'].attrs)
-            return self.format(func=func, **context)
-
-    return ScriptTemplate(SCRIPT_TEMPLATE)
+    version_file = read('pygameweb', '__init__.py')
+    version_match = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]',
+                              version_file, re.MULTILINE)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
 
 
-ScriptWriter.template = fast_entrypoint_script_template()
+def get_requirements():
+    """ returns list of requirements from requirements.txt files.
+    """
+    fnames = ['requirements.txt']
+    requirements = chain.from_iterable((open(fname) for fname in fnames))
+    requirements = list(set([l.strip() for l in requirements]) - {'-r requirements.txt'})
+    return requirements
+
+
 setup(
-    cmdclass={
-        'install': install,
-        'build': build,
-        'build_mo': build_mo,
-    },
-    data_files=[
-        ('share/icons/hicolor/scalable/actions', icon_files),
-        ('share/zsh/site-functions', comp_files),
-        *[('share/locale/{}/LC_MESSAGES'.format(lang),
-           ['build/locale/{}/LC_MESSAGES/udiskie.mo'.format(lang)])
-          for lang in languages],
+    name='pygameweb',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
     ],
+    data_files=[('.', ['alembic.ini'])],
+    license='BSD',
+    author='Rene Dudfield',
+    author_email='renesd@gmail.com',
+    description='Pygame.org website.',
+    include_package_data=True,
+    long_description=read('README.rst'),
+    package_dir={'pygameweb': 'pygameweb'},
+    packages=find_packages(),
+    package_data={'pygameweb': []},
+    url='https://github.com/pygame/pygameweb',
+    install_requires=get_requirements(),
+    version=get_version(),
+    entry_points={
+        'console_scripts': [
+            'pygameweb_front='
+                'pygameweb.run:run_front',
+            'pygameweb_generate_json='
+                'pygameweb.dashboard.generate_json:main',
+            'pygameweb_generate_static='
+                'pygameweb.dashboard.generate_static:main',
+            'pygameweb_launchpad='
+                'pygameweb.builds.launchpad_build_badge:check_pygame_builds',
+            'pygameweb_update_docs='
+                'pygameweb.builds.update_docs:update_docs',
+            'pygameweb_stackoverflow='
+                'pygameweb.builds.stackoverflow:download_stack_json',
+            'pygameweb_loadcomments='
+                'pygameweb.comment.models:load_comments',
+            'pygameweb_trainclassifier='
+                'pygameweb.comment.classifier_train:classify_comments',
+            'pygameweb_worker='
+                'pygameweb.tasks.worker:work',
+            'pygameweb_release_version_correct='
+                'pygameweb.builds.update_version_from_git:release_version_correct',
+            'pygameweb_github_releases='
+                'pygameweb.project.gh_releases:sync_github_releases',
+            'pygameweb_fixtures=' +
+                'pygameweb.fixtures:populate_db',
+        ],
+    },
 )
