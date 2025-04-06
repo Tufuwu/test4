a60037899cf5bc2c63409b354e0c7e4e5392e073
wu <wu@.com>
2025-04-06 23:21:44 +0800

    ciciui/django-db-logger/action

--- setup.py
+++ setup.py
@@ -1,78 +1,38 @@
-#! /usr/bin/env/python
-# vim:sw=4 ts=4 et:
-#
-# Copyright (c) 2015 Torchbox Ltd.
-# felicity@torchbox.com 2015-09-14
-#
-# Permission is granted to anyone to use this software for any purpose,
-# including commercial applications, and to alter it and redistribute it
-# freely. This software is provided 'as-is', without any express or implied
-# warranty.
-#
+import os
+from setuptools import setup
 
-import subprocess
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
 
-from setuptools import find_packages, setup
-
-
-def get_git_revision_hash():
-    try:
-        git_hash = subprocess.check_output(["git", "rev-parse", "HEAD"])
-    except subprocess.CalledProcessError:
-        return 'main'
-    else:
-        return git_hash.decode("ascii").splitlines()[0]
-
-
-README = "https://github.com/torchbox/wagtail-markdown/blob/{hash}/README.md"
-README = README.format(hash=get_git_revision_hash())
-
-
-INSTALL_REQUIRES = [
-    'Markdown>=3,<4',
-    'bleach>=3.3.0,<4',
-    'Wagtail>=2.0',
-]
-
-
-TESTING_REQUIRES = [
-    "dj_database_url==0.5.0",
-]
-
-
-CLASSIFIERS = [
-    "Development Status :: 4 - Beta",
-    "Environment :: Web Environment",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: zlib/libpng License",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3",
-    "Framework :: Django",
-    "Framework :: Django :: 2.2",
-    "Framework :: Django :: 3.0",
-    "Framework :: Django :: 3.1",
-    "Framework :: Wagtail",
-    "Framework :: Wagtail :: 2",
-]
-
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="wagtail-markdown",
-    version="0.7.0-alpha",
-    description="Markdown support for Wagtail",
-    long_description="Provides Markdown page field and streamfield block for "
-    "Wagtail. More info: {}".format(README),
-    author="Felicity Tarnell",
-    author_email="felicity@torchbox.com",
-    url="https://github.com/torchbox/wagtail-markdown",
-    project_urls={
-        "Changelog": "https://github.com/torchbox/wagtail-markdown/blob/main/CHANGELOG.md",
-    },
-    install_requires=INSTALL_REQUIRES,
-    license="zlib",
-    packages=find_packages(),
+    name='django-db-logger',
+    version='0.1.11',
+    packages=['django_db_logger', 'django_db_logger.migrations'],
     include_package_data=True,
-    classifiers=CLASSIFIERS,
-    extras_require={"testing": TESTING_REQUIRES},
+    license='MIT License',
+    description='Django logging in database',
+    long_description=README,
+    url='https://github.com/CiCiUi/django-db-logger',
+    author='zhangshine',
+    author_email='zhangshine0125@gmail.com',
+    install_requires=['django>=1.9', 'six'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
 )
