e7a89fccf0683f6de9dc606476d8cbaecaf330db
wu <wu@.com>
2025-04-06 23:19:08 +0800

    aosingh/lexpy/action

--- aioapns/exceptions.py
+++
@@ -1,6 +0,0 @@
-class ConnectionClosed(Exception):
-    pass
-
-
-class ConnectionError(Exception):
-    pass
--- setup.py
+++ setup.py
@@ -1,33 +1,60 @@
-#!/usr/bin/env python
+from os import path
+from setuptools import setup, find_packages
 
-from setuptools import find_packages, setup
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+
+DISTNAME = 'lexpy'
+
+AUTHOR = 'Abhishek Singh'
+MAINTAINER = 'Abhishek Singh'
+MAINTAINER_EMAIL = 'abhishek.singh20141@gmail.com'
+DESCRIPTION = 'Python package for lexicon.'
+LICENSE = 'GNU GPLv3'
+URL = 'https://github.com/aosingh/lexpy'
+VERSION = '1.0.0'
+
+PACKAGES = ['lexpy']
+
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Topic :: Text Processing :: Linguistic',
+    'Topic :: Text Processing :: Indexing',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Operating System :: POSIX :: Linux',
+    'Operating System :: Unix',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: MacOS'
+]
+keywords = 'trie suffix-trees lexicon directed-acyclic-word-graph dawg'
+
 
 setup(
-    name="aioapns",
-    version="2.0.2",
-    description="An efficient APNs Client Library for Python/asyncio",
-    long_description=open("README.rst").read(),
-    platforms="all",
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Intended Audience :: Developers",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: POSIX",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Development Status :: 5 - Production/Stable",
-    ],
-    license="Apache License, Version 2.0",
-    author="Alexander Tikhonov",
-    author_email="random.gauss@gmail.com",
-    url="https://github.com/Fatal1ty/aioapns",
+    name=DISTNAME,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=MAINTAINER_EMAIL,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    description=DESCRIPTION,
+    license=LICENSE,
+    url=URL,
+    version=VERSION,
     packages=find_packages(exclude=("tests",)),
-    install_requires=[
-        "h2>=4.0.0",
-        "pyOpenSSL>=17.5.0",
-        "pyjwt>=2.0.0",
-    ],
+    package_dir={'lexpy': 'lexpy'},
+    include_package_data=True,
+    classifiers=classifiers,
+    keywords=keywords.split(),
 )
