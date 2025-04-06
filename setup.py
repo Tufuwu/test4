bf267efb2259d8749a783338fc2ab43eca172c47
wu <wu@.com>
2025-04-06 23:15:17 +0800

    adobe/ops-cli/action

--- setup.py
+++ setup.py
@@ -1,121 +1,62 @@
-#! /usr/bin/python
-"""pytest-dependency - Manage dependencies of tests
+# Copyright 2019 Adobe. All rights reserved.
+# This file is licensed to you under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License. You may obtain a copy
+# of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-This pytest plugin manages dependencies of tests.  It allows to mark
-some tests as dependent from other tests.  These tests will then be
-skipped if any of the dependencies did fail or has been skipped.
-"""
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+# OF ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
 
 import os
-import os.path
-import re
-import stat
-import string
-import setuptools
-from distutils.cmd import Command as du_cmd
-import distutils.command.sdist
-import distutils.log
-from setuptools import setup
-import setuptools.command.build_py
+import sys
+
 try:
-    import setuptools_scm
-    version = setuptools_scm.get_version()
-    with open(".version", "wt") as f:
-        f.write(version)
-except (ImportError, LookupError):
-    try:
-        with open(".version", "rt") as f:
-            version = f.read()
-    except (OSError, IOError):
-        distutils.log.warn("warning: cannot determine version number")
-        version = "UNKNOWN"
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
 
-doc_string = __doc__
+with open('README.md') as f:
+    _readme = f.read()
 
-class copy_file_mixin:
-    """Distutils copy_file() mixin.
-
-    Inject a custom version version of the copy_file() method that
-    does some substitutions on the fly into distutils command class
-    hierarchy.
-    """
-    Subst_srcs = {"src/pytest_dependency.py"}
-    Subst = {'DOC': doc_string, 'VERSION': version}
-    def copy_file(self, infile, outfile, preserve_mode=1, preserve_times=1,
-                  link=None, level=1):
-        if infile in self.Subst_srcs:
-            fstat = os.stat(infile)
-            if os.path.basename(outfile) == os.path.basename(infile):
-                distutils.log.info("copying (with substitutions) %s -> %s",
-                                   infile, os.path.dirname(outfile))
-            else:
-                distutils.log.info("copying (with substitutions) %s -> %s",
-                                   infile, outfile)
-            if not self.dry_run:
-                if os.path.exists(outfile):
-                    os.unlink(outfile)
-                with open(infile, "rt") as sf, open(outfile, "wt") as df:
-                    df.write(string.Template(sf.read()).substitute(self.Subst))
-                if preserve_mode:
-                    os.chmod(outfile, stat.S_IMODE(fstat[stat.ST_MODE]))
-            return (outfile, 1)
-        else:
-            # Note: can't use super() with Python 2.
-            return du_cmd.copy_file(self, infile, outfile,
-                                    preserve_mode=preserve_mode,
-                                    preserve_times=preserve_times,
-                                    link=link, level=level)
-
-class build_py(copy_file_mixin, setuptools.command.build_py.build_py):
-    pass
-
-# Note: Do not use setuptools for making the source distribution,
-# rather use the good old distutils instead.
-# Rationale: https://rhodesmill.org/brandon/2009/eby-magic/
-class sdist(copy_file_mixin, distutils.command.sdist.sdist):
-    pass
-
+_mydir = os.path.abspath(os.path.dirname(sys.argv[0]))
+_requires = [r for r in open(os.path.sep.join((_mydir, 'requirements.txt')), "r").read().split('\n') if len(r) > 1]
 setup(
-    name='pytest-dependency',
-    version=version,
-    description='Manage dependencies of tests',
-    author='Rolf Krahl',
-    author_email='rolf@rotkraut.de',
-    maintainer='Rolf Krahl',
-    maintainer_email='rolf@rotkraut.de',
-    url='https://github.com/RKrahl/pytest-dependency',
-    license='Apache Software License 2.0',
-    long_description=doc_string,
-    project_urls={
-        'Documentation': 'https://pytest-dependency.readthedocs.io/',
-        'Source Code': 'https://github.com/RKrahl/pytest-dependency',
-    },
-    package_dir = {'': 'src'},
-    py_modules=['pytest_dependency'],
-    install_requires=['pytest >= 3.7.0'],
+    name='ops-cli',
+    version='2.0.6',
+    description='Ops - wrapper for Terraform, Ansible, and SSH for cloud automation',
+    long_description=_readme + '\n\n',
+    long_description_content_type='text/markdown',
+    url='https://github.com/adobe/ops-cli',
+    python_requires='>=3.5',
+    author='Adobe',
+    author_email='noreply@adobe.com',
+    license='Apache2',
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Pytest',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Testing',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: HTML'
     ],
-    entry_points={
-        'pytest11': [
-            'dependency = pytest_dependency',
-        ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    package_data={
+        '': ['data/ansible/*', 'data/ansible/tasks/*', 'data/ssh/*', 'data/terraform/*']
     },
-    cmdclass = {'build_py': build_py, 'sdist': sdist},
+    install_requires=_requires,
+    entry_points={
+        'console_scripts': [
+            'ops = ops.main:run'
+        ]
+    }
 )
