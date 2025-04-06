2afaf57e0d56d90e25c5dcb3458780d3a3680423
wu <wu@.com>
2025-04-07 01:44:13 +0800

    daler/gffutils/action

--- setup.py
+++ setup.py
@@ -1,43 +1,38 @@
-import os
-import warnings
 
+import os
+import sys
 from setuptools import setup
 
-cur_dir = os.path.dirname(__file__)
-readme_file = os.path.join(cur_dir, 'README.md')
-with open(readme_file) as fh:
-    readme = fh.read()
-
-try:
-    from scout import __version__ as scout_version
-except ImportError:
-    scout_version = '0.0.0'
-    warnings.warn('Unable to determine scout library version!')
-
+version_py = os.path.join(os.path.dirname(__file__), 'gffutils', 'version.py')
+version = open(version_py).read().strip().split('=')[-1].replace('"', '')
+requirements = open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines()
 setup(
-    name='scout',
-    version=scout_version,
-    url='http://github.com/coleifer/scout/',
-    license='MIT',
-    author='Charles Leifer',
-    author_email='coleifer@gmail.com',
-    description='scout - a lightweight search server powered by SQLite',
-    packages=['scout'],
-    zip_safe=False,
-    platforms='any',
-    install_requires=[
-        'flask',
-        'peewee>=3.0.0'],
+    name='gffutils',
+    version=version,
+    install_requires=requirements,
+    packages=['gffutils', 'gffutils.scripts', 'gffutils.test',
+              'gffutils.test.data'],
+    scripts=['gffutils/scripts/gffutils-cli'],
+    author='Ryan Dale',
+    package_dir={'gffutils': 'gffutils'},
+    package_data = {'gffutils': ['test/data/*']},
+    description="Work with GFF and GTF files in a flexible "
+    "database framework",
+    author_email='dalerr@niddk.nih.gov',
+    url='https://github.com/daler/gffutils',
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python'],
-    py_modules=['scout_client'],
-    test_suite='scout.tests',
-    entry_points="""
-        [console_scripts]
-        scout=scout.server:main
-    """,
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
