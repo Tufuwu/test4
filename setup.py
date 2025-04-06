daf13022c425bebedbafc7f209ace1517163bfab
wu <wu@.com>
2025-04-07 02:42:28 +0800

    weldr/lorax/action

--- setup.py
+++ setup.py
@@ -1,79 +1,48 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2003, Taro Ogawa.  All Rights Reserved.
-# Copyright (c) 2013, Savoir-faire Linux inc.  All Rights Reserved.
+#!/usr/bin/python2
 
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# Lesser General Public License for more details.
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-# MA 02110-1301 USA
-
-import re
-from io import open
-
-from setuptools import find_packages, setup
-
-PACKAGE_NAME = "num2words"
-
-CLASSIFIERS = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: GNU Library or Lesser General Public License '
-    '(LGPL)',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Topic :: Software Development :: Internationalization',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    'Topic :: Software Development :: Localization',
-    'Topic :: Text Processing :: Linguistic',
-]
-
-LONG_DESC = open('README.rst', 'rt', encoding="utf-8").read() + '\n\n' + \
-            open('CHANGES.rst', 'rt', encoding="utf-8").read()
+from distutils.core import setup
+import os
+import sys
 
 
-def find_version(fname):
-    """Parse file & return version number matching 0.0.1 regex
-    Returns str or raises RuntimeError
-    """
-    version = ''
-    with open(fname, 'r', encoding="utf-8") as fp:
-        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
-        for line in fp:
-            m = reg.match(line)
-            if m:
-                version = m.group(1)
-                break
-    if not version:
-        raise RuntimeError('Cannot find version information')
-    return version
+# config file
+data_files = [("/etc/lorax", ["etc/lorax.conf"]),
+              ("/usr/lib/tmpfiles.d/", ["systemd/lorax.conf"])]
+
+# shared files
+for root, dnames, fnames in os.walk("share"):
+    for fname in fnames:
+        data_files.append((root.replace("share", "/usr/share/lorax", 1),
+                           [os.path.join(root, fname)]))
+
+# executable
+data_files.append(("/usr/sbin", ["src/sbin/lorax", "src/sbin/mkefiboot",
+                                 "src/sbin/livemedia-creator", "src/sbin/mkksiso"]))
+data_files.append(("/usr/bin",  ["src/bin/image-minimizer",
+                                 "src/bin/composer-cli"]))
+
+# get the version
+sys.path.insert(0, "src")
+try:
+    import pylorax.version
+except ImportError:
+    vernum = "devel"
+else:
+    vernum = pylorax.version.num
+finally:
+    sys.path = sys.path[1:]
 
 
-setup(
-    name=PACKAGE_NAME,
-    version=find_version("bin/num2words"),
-    description='Modules to convert numbers to words. Easily extensible.',
-    long_description=LONG_DESC,
-    license='LGPL',
-    author='Taro Ogawa <tso at users sourceforge net>',
-    author_email='tos@users.sourceforge.net',
-    maintainer='Savoir-faire Linux inc.',
-    maintainer_email='istvan.szalai@savoirfairelinux.com',
-    keywords=' number word numbers words convert conversion i18n '
-             'localisation localization internationalisation '
-             'internationalization',
-    url='https://github.com/savoirfairelinux/num2words',
-    packages=find_packages(exclude=['tests']),
-    test_suite='tests',
-    classifiers=CLASSIFIERS,
-    scripts=['bin/num2words'],
-    install_requires=["docopt>=0.6.2"],
-    tests_require=['delegator.py'],
-)
+setup(name="lorax",
+      version=vernum,
+      description="Lorax",
+      long_description="Tools for creating bootable images, including the Anaconda boot.iso",
+      author="Martin Gracik, Will Woods <wwoods@redhat.com>, Brian C. Lane <bcl@redhat.com>",
+      author_email="bcl@redhat.com",
+      url="http://www.github.com/weldr/lorax/",
+      download_url="http://www.github.com/weldr/lorax/releases/",
+      license="GPLv2+",
+      packages=["pylorax", "composer", "composer.cli"],
+      package_dir={"" : "src"},
+      data_files=data_files
+      )
