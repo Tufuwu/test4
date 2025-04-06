f6fb5a89b6f1219654231571b8d56cab78f69b2f
wu <wu@.com>
2025-04-07 02:20:14 +0800

    tartley/colorama/action

--- setup.py
+++ setup.py
@@ -1,133 +1,67 @@
-# ------------------------------------------------------------------
-# Copyright (c) 2020 PyInstaller Development Team.
-#
-# This file is distributed under the terms of the GNU General Public
-# License (version 2.0 or later).
-#
-# The full license is available in LICENSE.GPL.txt, distributed with
-# this software.
-#
-# SPDX-License-Identifier: GPL-2.0-or-later
-# ------------------------------------------------------------------
-from setuptools import setup, Command
+#!/usr/bin/env python
+# Copyright Jonathan Hartley 2013. BSD 3-Clause license, see LICENSE file.
+
+from __future__ import with_statement
+
+from io import open
 import os
-import datetime
+import re
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-DIR = os.path.dirname(__file__)
+
+NAME = 'colorama'
 
 
-class BumpVersion(Command):
-    """Bump the package version in the source files."""
-    description = 'Bump the version in all registered files.'
-    user_options = [
-        ('major', None,
-         'Bump the major (leftmost) version number. If specified, build/minor version numbers will be set to 0.'),
-        ('minor', None, 'Bump the minor (middle) version number. If specified, the build NO will be set to 0.'),
-        ('build', None, 'Bump the build (rightmost) version number. (Default)')
-    ]
-    
-    def get_version_tuple(self, str_ver):
-        return list(int(x) for x in str_ver.split('.'))
-    
-    def initialize_options(self):
-        self.major = False
-        self.minor = False
-        self.build = True
-    
-    def finalize_options(self):
-        if self.major:
-            self.major = True
-            self.minor = False
-            self.build = False
-        elif self.minor:
-            self.major = False
-            self.minor = True
-            self.build = False
-        else:
-            self.major = False
-            self.minor = False
-            self.build = True
-    
-    def run(self):
-        import re
-        # REGEX:
-        #  [0-9]+  - First part of version: [4].0.1
-        #  [.][0-9]*  - Second part of version: 4[.0].1
-        #  [.]*[0-9]*  - Third - and optional - part of version
-        #  [^.]$  - The entire string must not end with a dot
-        version_regex = re.compile('[0-9]+[.][0-9]*[.]*[0-9]*[^.]$')
-        
-        # List of ABSOLUTE file paths of files to bump
-        files = [
-            os.path.abspath(os.path.join(DIR, 'src/_pyinstaller_hooks_contrib/__init__.py'))
-        ]
-        for file in files:
-            old_file = open(file).readlines()
-            changed = False
-            
-            for i in range(len(old_file)):
-                # Get rid of line endings, if they exist
-                line = old_file[i].replace('\n', '')
-                
-                # If the line starts with version, try and bump it
-                if line.startswith('__version__'):
-                    print('Line {ln} in {file} appears to be a version. Attempting to bump...'.format(ln=i, file=file))
-                    line = line.split(' = ')[1].replace(' ', '').replace("'", '')
-                    m = version_regex.match(line)
-                    if m:
-                        # Convert a "valid" version to a tuple of ints
-                        ver = self.get_version_tuple(m.string)
-                        print(ver)
-                        
-                        # If the tuple isn't valid - not len(3) - then it's not a valid version
-                        if len(ver) not in (2, 3):
-                            print('Invalid version number. Skipping...')
-                            continue
-                        
-                        if len(ver) == 3:
-                            if self.major:
-                                ver[0] += 1
-                                ver[1] = 0
-                                ver[2] = 0
-                                
-                            elif self.minor:
-                                ver[1] += 1
-                                ver[2] = 0
-                            else:
-                                ver[2] += 1
-                        else:
-                            if datetime.datetime.now().year != ver[0]:
-                                ver[0] = datetime.datetime.now().year
-                                ver[1] = 0
-                            else:
-                                ver[1] += 1
-                        
-                        ver = '.'.join(str(x) for x in ver)
-                        
-                        old_file[i] = old_file[i].replace(m.string, ver)
-                        print('Version bumped from {} to {}.'.format(m.string, ver))
-                        changed = True
-                    else:
-                        print('No version found - {file}:{ln}'.format(ln=i, file=file))
-            
-            if changed:
-                # Write the changes to the file
-                with open(file, 'w') as f:
-                    f.writelines(old_file)
-                # And print that to the console.
-                print('Changes written to {}'.format(file))
-                        
+def read_file(path, encoding='ascii'):
+    with open(os.path.join(os.path.dirname(__file__), path),
+              encoding=encoding) as fp:
+        return fp.read()
+
+def _get_version_match(content):
+    # Search for lines of the form: # __version__ = 'ver'
+    regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    version_match = re.search(regex, content, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+def get_version(path):
+    return _get_version_match(read_file(path))
 
 setup(
-    setup_requires="setuptools >= 30.3.0",
-    entry_points={
-        'pyinstaller40': [
-            'hook-dirs = _pyinstaller_hooks_contrib.hooks:get_hook_dirs',
-            'tests = _pyinstaller_hooks_contrib.tests:get_test_dirs'
-        ]
-    },
-    cmdclass={
-        'bump': BumpVersion
-    },
-    long_description_content_type='text/markdown'
+    name=NAME,
+    version=get_version(os.path.join('colorama', '__init__.py')),
+    description='Cross-platform colored terminal text.',
+    long_description=read_file('README.rst'),
+    keywords='color colour terminal text ansi windows crossplatform xplatform',
+    author='Jonathan Hartley',
+    author_email='tartley@tartley.com',
+    maintainer='Arnon Yaari',
+    url='https://github.com/tartley/colorama',
+    license='BSD',
+    packages=[NAME],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    # see classifiers https://pypi.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Terminals',
+    ]
 )
