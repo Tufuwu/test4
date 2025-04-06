be5bb13e7eab563cbe1c9f75d25b35b063505570
wu <wu@.com>
2025-04-06 23:18:32 +0800

    okpy/ok-client/action

--- setup.py
+++ setup.py
@@ -1,45 +1,44 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import client
 
+VERSION = client.__version__
 
 setup(
-        name='nose2pytest',
-        version='1.0.8',
-        packages=['nose2pytest'],
-        # py_modules=['assert_tools', 'nose2pytest'],
-        entry_points={
-            'console_scripts': [
-                'nose2pytest = nose2pytest.script:main',
-            ],
-            'pytest11': ['pytest_nose_assert_tools = nose2pytest.assert_tools'],
-        },
-        url='https://github.com/schollii/nose2pytest',
-        license='BSD-3',
-        author='Oliver Schoenborn',
-        author_email='oliver.schoenborn@gmail.com',
-        description='Convert nose.tools.assert_ calls found in your Nose test modules into raw asserts for pytest',
-        keywords='nose to pytest conversion',
+    name='okpy',
+    version=VERSION,
+    author='John Denero, Soumya Basu, Stephen Martinis, Sharad Vikram, Albert Wu',
+    # author_email='',
+    description=('ok.py supports programming projects by running tests, '
+                'tracking progress, and assisting in debugging.'),
+    # long_description=long_description,
+    url='https://github.com/okpy/ok-client',
+    # download_url='https://github.com/okpy/ok/releases/download/v{}/ok'.format(VERSION),
 
-        python_requires='>=3.5',
-        classifiers=[
-            # How mature is this project? Common values are
-            #   3 - Alpha
-            #   4 - Beta
-            #   5 - Production/Stable
-            'Development Status :: 5 - Production/Stable',
-
-            # Indicate who your project is intended for
-            'Intended Audience :: Developers',
-            'Topic :: Software Development :: Testing',
-
-            # Pick your license as you wish (should match "license" above)
-            'License :: OSI Approved :: BSD License',
-
-            # Specify the Python versions you support here.
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3 :: Only',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
+    license='Apache License, Version 2.0',
+    keywords=['education', 'autograding'],
+    packages=find_packages(include=[
+        'client',
+        'client.*',
+    ]),
+    package_data={
+        'client': ['config.ok'],
+    },
+    # install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'ok=client.cli.ok:main',
+            'ok-publish=client.cli.publish:main',
+            'ok-lock=client.cli.lock:main',
+            'ok-test=client.cli.test:main',
         ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        'requests==2.12.4',
+        'coverage==4.4'
+    ],
 )
