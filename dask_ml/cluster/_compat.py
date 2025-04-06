41325a7d68eaa1b4dc9e974ead382120be21efcc
wu <wu@.com>
2025-04-07 00:12:54 +0800

    alecthomas/injector/action

--- dask_ml/cluster/_compat.py
+++
@@ -1,3 +0,0 @@
-from sklearn.cluster._kmeans import _k_init
-
-__all__ = ["_k_init"]
--- setup.py
+++ setup.py
@@ -1,73 +1,86 @@
-import os
-from codecs import open
-
-from setuptools import find_packages, setup
-
-here = os.path.dirname(__file__)
+from setuptools import setup, Command
+import sys
+import warnings
 
 
-# Get the long description from the README file
-with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
-    long_description = f.read()
+warnings.filterwarnings("always", module=__name__)
 
-install_requires = [
-    "dask[array,dataframe]>=2.4.0",
-    "distributed>=2.4.0",
-    "numba",
-    "numpy>=1.17.3",
-    "pandas>=0.24.2",
-    "scikit-learn>=0.23",
-    "scipy",
-    "dask-glm>=0.2.0",
-    "multipledispatch>=0.4.9",
-    "packaging",
-]
 
-# Optional Requirements
-doc_requires = ["sphinx", "numpydoc", "sphinx-rtd-theme", "nbsphinx", "sphinx-gallery"]
-test_requires = [
-    "black",
-    "coverage",
-    "flake8",
-    "isort",
-    "pytest",
-    "pytest-cover",
-    "pytest-mock",
-]
-dev_requires = doc_requires + test_requires
-xgboost_requires = ["dask-xgboost", "xgboost"]
-complete_requires = xgboost_requires
+def obtain_requirements(file_name):
+    with open(file_name) as fd_in:
+        for line in fd_in:
+            if '#' not in line:
+                yield line.strip()
 
-extras_require = {
-    "docs": doc_requires,
-    "test": test_requires,
-    "dev": dev_requires,
-    "xgboost": xgboost_requires,
-    "complete": complete_requires,
-}
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+
+        errno = subprocess.call([sys.executable, '-m', 'pytest'])
+        raise SystemExit(errno)
+
+
+def read_injector_variable(name):
+    prefix = '%s = ' % (name,)
+    with open('injector/__init__.py') as f:
+        for line in f:
+            if line.startswith(prefix):
+                return line.replace(prefix, '').strip().strip("'")
+    raise AssertionError('variable %s not found' % (name,))
+
+
+version = read_injector_variable('__version__')
+version_tag = read_injector_variable('__version_tag__')
+
+
+requirements = list(obtain_requirements('requirements.txt'))
+requirements_dev = list(obtain_requirements('requirements-dev.txt'))
+
+
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    warnings.warn('Could not locate pandoc, using Markdown long_description.', ImportWarning)
+    with open('README.md') as f:
+        long_description = f.read()
+
+description = long_description.splitlines()[0].strip()
+
 
 setup(
-    name="dask-ml",
-    description="A library for distributed and parallel machine learning",
+    name='injector',
+    url='https://github.com/alecthomas/injector',
+    download_url='https://pypi.org/project/injector/',
+    version=version,
+    options=dict(egg_info=dict(tag_build=version_tag)),
+    description=description,
     long_description=long_description,
-    url="https://github.com/dask/dask-ml",
-    author="Tom Augspurger",
-    author_email="taugspurger@anaconda.com",
-    license="BSD",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Topic :: Database",
-        "Topic :: Scientific/Engineering",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+    license='BSD',
+    platforms=['any'],
+    packages=['injector'],
+    package_data={'injector': ['py.typed']},
+    author='Alec Thomas',
+    author_email='alec@swapoff.org',
+    cmdclass={'test': PyTest},
+    extras_require={'dev': requirements_dev},
+    keywords=[
+        'Dependency Injection',
+        'DI',
+        'Dependency Injection framework',
+        'Inversion of Control',
+        'IoC',
+        'Inversion of Control container',
     ],
-    packages=find_packages(exclude=["docs", "tests", "tests.*", "docs.*"]),
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
-    install_requires=install_requires,
-    extras_require=extras_require,
-    python_requires=">=3.6",
+    install_requires=requirements,
 )
