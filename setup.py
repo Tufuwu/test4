4cf7d65c535730473988fe65f4bbf8ec48c51b72
wu <wu@.com>
2025-04-06 20:35:08 +0800

    roskakori/pygount/action

--- setup.py
+++ setup.py
@@ -1,61 +1,58 @@
-# setuptools installation of POW
-# Copyright (c) 2010 Oliver Beckstein <orbeckst@gmail.com>
-# Released under the GNU Public License 3 (or higher, your choice)
+"""
+Setup for pygount.
 
+Developer cheat sheet
+---------------------
 
+Tag a release (simply replace ``1.x.x`` with the current version number)::
+
+  $ git tag -a -m "Tagged version 1.x.x" v1.x.x
+  $ git push --tags
+
+Upload release to PyPI::
+
+  $ python3 setup.py bdist_wheel
+  $ twine upload dist/*.whl
+"""
+# Copyright (c) 2016-2020, Thomas Aglassinger.
+# All rights reserved. Distributed under the BSD License.
+import os
 from setuptools import setup, find_packages
 
-# Dynamically calculate the version based on VERSION.
-version = __import__('mdpow.version').get_version()
+from pygount import __version__
 
-setup(name="MDPOW",
-      version=version,
-      description="A library for computing solvation/water partitioning coefficients using molecular dynamics simulations",
-      long_description=open("README.rst").read(),
-      author="Oliver Beckstein",
-      author_email="orbeckst@gmail.com",
-      license="GPLv3",
-      url="https://github.com/Becksteinlab/MDPOW",
-      keywords="science Gromacs analysis 'molecular dynamics'",
-      classifiers=[
-          "Development Status :: 4 - Beta",
-          "Environment :: Console",
-          "Intended Audience :: Science/Research",
-          "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-          "Operating System :: POSIX",
-          "Programming Language :: Python :: 2.7",
-          "Topic :: Scientific/Engineering :: Chemistry",
-          "Topic :: Scientific/Engineering :: Physics",
-      ],
-      packages=find_packages(exclude=['examples']),
-      scripts=['scripts/mdpow-pow',
-               'scripts/mdpow-pcw',
-               'scripts/mdpow-ghyd',
-               'scripts/mdpow-check',
-               'scripts/mdpow-rebuild-fep',
-               'scripts/mdpow-rebuild-simulation',
-               'scripts/mdpow-equilibrium',
-               'scripts/mdpow-fep',
-               'scripts/mdpow-cfg2yaml.py',
-               'scripts/mdpow-solvationenergy',
-               'scripts/mdpow-get-runinput'
-      ],
-      package_data={'mdpow': ['top/*.dat', 'top/*.gro', 'top/*.itp',
-                              'top/oplsaa.ff/*',
-                              'top/charmm36-mar2019.ff/*',
-                              'top/amber99sb.ff/*',
-                              'templates/*'], },
-      install_requires=['numpy>=1.6', 'scipy',
-                        'pyyaml',
-                        'GromacsWrapper>=0.5.1',
-                        'numkit',
-                        'six',
-                        'mdanalysis',
-                        'alchemlyb',
-                        'pandas',
-                        'pymbar',
-      ],
-      #setup_requires=['pytest-runner',],
-      tests_require=['pytest', 'pybol', 'py'],
-      zip_safe=True,
+
+# Read the long description from the README file.
+_setup_folder = os.path.dirname(__file__)
+with open(os.path.join(_setup_folder, "README.md"), encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="pygount",
+    version=__version__,
+    description="count source lines of code (SLOC) using pygments",
+    long_description=long_description,
+    url="https://github.com/roskakori/pygount",
+    author="Thomas Aglassinger",
+    author_email="roskakori@users.sourceforge.net",
+    license="BSD",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development",
+    ],
+    keywords=["code analysis", "count", "SLOC"],
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["pygments>=2.0"],
+    python_requires=">=3.5",
+    entry_points={"console_scripts": ["pygount=pygount.command:main"]},
 )
