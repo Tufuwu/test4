8e3e3cb89568888b54df4c57e6bf8df6ddd00d07
wu <wu@.com>
2025-04-06 23:25:02 +0800

    tompollard/tableone/action

--- setup.py
+++ setup.py
@@ -1,60 +1,93 @@
-#!/usr/bin/env python
+# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+from pathlib import Path
 
-DISTNAME = 'tract_querier'
-DESCRIPTION = \
-    'WMQL: Query language for automatic tract extraction from '\
-    'full-brain tractographies with '\
-    'a registered template on top of them'
-LONG_DESCRIPTION = open('README.md').read()
-MAINTAINER = 'Demian Wassermann'
-MAINTAINER_EMAIL = 'demian@bwh.harvard.edu'
-URL = 'http://demianw.github.io/tract_querier'
-LICENSE = open('license.rst').read()
-DOWNLOAD_URL = 'https://github.com/demianw/tract_querier'
-VERSION = '0.1'
+# To use a consistent encoding
+from codecs import open
+from os import path
 
+here = path.abspath(path.dirname(__file__))
 
-if __name__ == "__main__":
-    setup(
-        name=DISTNAME,
-        maintainer=MAINTAINER,
-        maintainer_email=MAINTAINER_EMAIL,
-        description=DESCRIPTION,
-        license=LICENSE,
-        url=URL,
-        version=VERSION,
-        download_url=DOWNLOAD_URL,
-        long_description=LONG_DESCRIPTION,
-        requires=[
-            'numpy(>=1.6)',
-            'nibabel(>=1.3)'
+# Get the long description from the README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='tableone',
+
+    # Versions should comply with PEP440. For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.7.11',
+
+    description='TableOne',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    # The project's main homepage.
+    url='https://github.com/tompollard/tableone',
+
+    # Author details
+    author='Tom Pollard',
+    author_email='tpollard@mit.edu',
+
+    # Choose your license
+    license='MIT',
+
+    # What does your project relate to?
+    keywords='Table one Table 1 clinical research population cohort',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=['tableone'],
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    # py_modules=['tableone', 'modality'],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'numpy>=1.23.2',
+        'pandas>=1.4.3',
+        'scipy>=1.9.0',
+        'statsmodels>=0.13.2',
+        'tabulate>=0.8.10'
         ],
-        classifiers=[
-            'Intended Audience :: Science/Research',
-            'Programming Language :: Python',
-            'Topic :: Scientific/Engineering',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: POSIX',
-            'Operating System :: Unix',
-            'Operating System :: MacOS'
-        ],
-        scripts=[
-            'scripts/tract_querier',
-            'scripts/tract_math'
-        ],
-        test_suite='nose.collector',
-        data_files=[
-            ('data',
-             [
-                 'data/FreeSurfer.qry',
-                 'data/JHU_MNI_SS_WMPM_Type_I.qry',
-                 'data/JHU_MNI_SS_WMPM_Type_II.qry',
-                 'data/freesurfer_queries.qry',
-                 'data/mori_queries.qry',
-             ]
-             )
-        ],
-        include_package_data=True,
-        packages=find_packages(),
-    )
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # package_data={'wfdb': ['wfdb.config'],
+    # },
+    include_package_data=True,
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+    # data_files=[('config', ['wfdb.config'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
+
+)
