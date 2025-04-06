3c5f3c4960bb47e7f7999e609d709ac89da862ec
wu <wu@.com>
2025-04-07 00:40:40 +0800

    elmotec/circlify/action

--- setup.py
+++ setup.py
@@ -1,30 +1,45 @@
-import setuptools
+#!/usr/bin/env python
+# encoding: utf-8
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+"""Packaging script."""
 
-import versioneer
+import os
 
-setuptools.setup(
-    name="removestar",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    author="Aaron Meurer",
-    author_email="asmeurer@gmail.com",
-    description="A tool to automatically replace 'import *' imports with explicit imports in files",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://www.asmeurer.com/removestar/",
-    packages=setuptools.find_packages(),
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme = open(os.path.join(here, "README.rst")).read()
+
+setup(
+    name="circlify",
+    description="Circle packing algorithm for Python",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    version="0.14.0",
+    author="Elmotec",
+    author_email="elmotec@gmx.com",
+    license="MIT",
+    keywords="circle packing enclosure hierarchy graph display visualization",
+    url="http://github.com/elmotec/circlify",
+    py_modules=["circlify"],
+    test_suite="tests",
+    setup_requires=[],
+    tests_require=[],
+    python_requires=">=3.5",
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Intended Audience :: Developers",
     ],
-    entry_points={'console_scripts': [ 'removestar = removestar.__main__:main']},
-    python_requires= '>=3.6',
-    install_requires=[
-        'pyflakes'
-    ],
-    license='MIT',
 )
