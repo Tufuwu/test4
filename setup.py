7f0ce06373294e57ef10cde6bccc82cd6e200065
wu <wu@.com>
2025-04-07 00:37:26 +0800

    craigds/django-typed-models/action

--- setup.py
+++ setup.py
@@ -1,63 +1,38 @@
-from importlib.machinery import SourceFileLoader
-from pathlib import Path
+#!/usr/bin/env python
 from setuptools import setup
+from os.path import abspath, dirname, join
 
-description = 'Static site generator'
-THIS_DIR = Path(__file__).resolve().parent
-try:
-    long_description = '\n\n'.join([
-        THIS_DIR.joinpath('README.md').read_text(),
-        # THIS_DIR.joinpath('HISTORY.md').read_text()
-    ])
-except FileNotFoundError:
-    long_description = description + '.\n\nSee https://harrier.helpmanual.io/ for documentation.'
 
-# avoid loading the package before requirements are installed:
-version = SourceFileLoader('version', 'harrier/version.py').load_module()
+def read_relative_file(filename):
+    """Returns contents of the given file, whose path is supposed relative
+    to this module."""
+    with open(join(dirname(abspath(__file__)), filename)) as f:
+        return f.read()
+
 
 setup(
-    name='harrier',
-    version=version.VERSION,
-    description=description,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    name="django-typed-models",
+    description="""Sane single table model inheritance for Django""",
+    long_description=read_relative_file("README.rst"),
+    version=read_relative_file("VERSION").strip(),
+    author="Craig de Stigter",
+    author_email="craig@destigter.nz",
+    url="http://github.com/craigds/django-typed-models",
+    packages=["typedmodels"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Utilities",
     ],
-    keywords='css,sass,scss,jinja,jinja2,build,static,static site generator',
-    author='Samuel Colvin',
-    author_email='s@muelcolvin.com',
-    url='https://github.com/samuelcolvin/harrier',
-    license='MIT',
-    packages=['harrier'],
-    python_requires='>=3.6',
-    zip_safe=True,
-    entry_points="""
-        [console_scripts]
-        harrier=harrier.cli:cli
-    """,
-    install_requires=[
-        'Jinja2>=2.8',
-        'Pygments>=2.2.0',
-        'aiohttp-devtools>=0.10, <=0.11',
-        'click>=6.2',
-        'devtools>=0.3',
-        'grablib>=0.7.4',
-        'libsass>=0.14.5',
-        'misaka>=2.1.0',
-        'pillow>=5.1.0',
-        'pydantic==0.20',
-        'ruamel.yaml>=0.15.37',
-        'watchgod>=0.2.0',
-    ]
 )
