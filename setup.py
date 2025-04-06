73869a1a5ba79f03a28b2759db8b6987b7a769c2
wu <wu@.com>
2025-04-07 00:34:32 +0800

    mozilla/foxpuppet/action

--- setup.py
+++ setup.py
@@ -1,47 +1,40 @@
-#!/usr/bin/env python
-import os, sys
-from setuptools import setup, find_packages
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+"""FoxPuppet setup file for packaging."""
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit(0)
-
-with open('README.rst', 'r') as f:
-    long_description = f.read()
-
-# Dynamically calculate the version based on swingtime.VERSION.
-version=__import__('swingtime').get_version()
+from setuptools import setup
 
 setup(
-    name='django-swingtime',
-    url='https://github.com/dakrauth/django-swingtime',
-    author='David A Krauth',
-    author_email='dakrauth@gmail.com',
-    description='A Django calendaring application.',
-    version=version,
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-    platforms=['any'],
-    license='MIT License',
-    python_requires='>=3.6, <4',
-    install_requires=['Django>=2.2,<4.0', 'python-dateutil==2.8.0'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
-        'Framework :: Django :: 3.1',
-        'Framework :: Django :: 3.2',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Office/Business :: Scheduling',
+    name="FoxPuppet",
+    use_scm_version=True,
+    description="Firefox user interface testing model for use with Selenium",
+    long_description=open("README.rst").read(),
+    author="Firefox Test Engineering",
+    author_email="firefox-test-engineering@mozilla.com",
+    url="https://github.com/mozilla/FoxPuppet",
+    packages=[
+        "foxpuppet",
+        "foxpuppet.windows",
+        "foxpuppet.windows.browser",
+        "foxpuppet.windows.browser.notifications",
     ],
-    packages=find_packages(),
-    package_data={'swingtime': ['locale/*/*/*.*',]},
-    zip_safe=False,
+    install_requires=["selenium>=3.0.2"],
+    setup_requires=["setuptools_scm"],
+    license="Mozilla Public License 2.0 (MPL 2.0)",
+    keywords="firefox ui testing mozilla selenium",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,77 +1,61 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+"""Configuration files for pytest."""
+
+import os
+
 import pytest
-from datetime import datetime
-from swingtime.models import EventType, Event, Occurrence
+
+from foxpuppet import FoxPuppet
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item, call):
+    """Add a report to the generated html report."""
+    pytest_html = item.config.pluginmanager.getplugin("html")
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, "extra", [])
+    driver = getattr(item, "_driver", None)
+    xfail = hasattr(report, "wasxfail")
+    failure = (report.skipped and xfail) or (report.failed and not xfail)
+    if driver is not None and failure:
+        with driver.context(driver.CONTEXT_CHROME):
+            screenshot = driver.get_screenshot_as_base64()
+            extra.append(pytest_html.extras.image(screenshot, "Screenshot (chrome)"))
+        report.extra = extra
+
+
+@pytest.fixture(scope="session")
+def webserver():
+    """Fixture that starts a local web server."""
+    from .webserver import WebServer
+
+    webserver = WebServer()
+    webserver.start()
+    yield webserver
+    webserver.stop()
+
 
 @pytest.fixture
-def work_type():
-    #2
-    return EventType.objects.create(abbr='work', label='Work')
-
-@pytest.fixture
-def play_type():
-    #1
-    return EventType.objects.create(abbr='play', label='Play')
+def browser(foxpuppet):
+    """First Firefox browser window opened."""
+    return foxpuppet.browser
 
 
 @pytest.fixture
-def occurence(work_type):
-    e = Event.objects.create(event_type=work_type, title='event')
-    return Occurrence.objects.create(
-        event=e,
-        start_time=datetime(2018, 3, 18, 16, 00), 
-        end_time=datetime(2018, 3, 18, 16, 45)
-    )
+def firefox_options(firefox_options):
+    """Fixture for configuring Firefox."""
+    if os.getenv("MOZREGRESSION_BINARY"):
+        firefox_options.binary = os.getenv("MOZREGRESSION_BINARY")
+    firefox_options.log.level = "trace"
+    # firefox_options.set_preference('devtools.chrome.enabled', True)
+    # firefox_options.set_preference('devtools.debugger.remote-enabled', True)
+    return firefox_options
 
 
 @pytest.fixture
-def events(play_type, work_type):
-    e = Event.objects.create(event_type=play_type, title='bravo')
-    Occurrence.objects.create(
-        event=e,
-        start_time=datetime(2008, 12, 11, 16, 00), 
-        end_time=datetime(2008, 12, 11, 16, 45)
-    )
-
-    e = Event.objects.create(event_type=work_type,  title='echo')
-    Occurrence.objects.create(
-        event=e,
-        start_time=datetime(2008, 12, 11, 17, 15), 
-        end_time=datetime(2008, 12, 11, 18, 00)
-    )
-
-    e = Event.objects.create(event_type=play_type, title='charlie')
-    Occurrence.objects.create(
-        event=e,
-        start_time=datetime(2008, 12, 11, 16, 15), 
-        end_time=datetime(2008, 12, 11, 17, 00)
-    )
-
-    e = Event.objects.create(event_type=work_type, title='foxtrot')
-    Occurrence.objects.create(
-        event=e,
-        start_time=datetime(2008, 12, 11, 16, 00), 
-        end_time=datetime(2008, 12, 11, 16, 45)
-    )
-
-    e = Event.objects.create(event_type=play_type, title='alpha')
-    Occurrence.objects.create(
-        event=e,
-        start_time=datetime(2008, 12, 11, 15, 30), 
-        end_time=datetime(2008, 12, 11, 17, 45)
-    )
-
-    e = Event.objects.create(event_type=work_type, title='zelda')
-    Occurrence.objects.create(
-        event=e,
-        start_time=datetime(2008, 12, 11, 15, 15), 
-        end_time=datetime(2008, 12, 11, 15, 45)
-    )
-
-    e = Event.objects.create(event_type=play_type, title='delta')
-    Occurrence.objects.create(
-        event=e,
-        start_time=datetime(2008, 12, 11, 16, 30), 
-        end_time=datetime(2008, 12, 11, 17, 15)
-    )
-
-    return Event.objects.all()
+def foxpuppet(selenium):
+    """Initialize the FoxPuppet object."""
+    return FoxPuppet(selenium)
