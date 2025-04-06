9fe2450bb89edc478fdc44780e69b263447d95a2
wu <wu@.com>
2025-04-06 23:22:02 +0800

    niwinz/django-jinja/action

--- setup.py
+++ setup.py
@@ -1,38 +1,66 @@
-import os
+#!/usr/bin/env python3
+
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
 setup(
-    name='django-db-logger',
-    version='0.1.11',
-    packages=['django_db_logger', 'django_db_logger.migrations'],
-    include_package_data=True,
-    license='MIT License',
-    description='Django logging in database',
-    long_description=README,
-    url='https://github.com/CiCiUi/django-db-logger',
-    author='zhangshine',
-    author_email='zhangshine0125@gmail.com',
-    install_requires=['django>=1.9', 'six'],
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    name = "django-jinja",
+    version = "2.9.1",
+    description = "Jinja2 templating language integrated in Django.",
+    long_description = open("README.rst").read(),
+    long_description_content_type='text/x-rst',
+    keywords = "django, jinja2",
+    author = "Andrey Antukh",
+    author_email = "niwi@niwi.be",
+    maintainer = "Asif Saif Uddin",
+    maintainer_email = "auvipy@gmail.com",
+    url = "https://github.com/niwinz/django-jinja",
+    license = "BSD",
+    packages = [
+        "django_jinja",
+        "django_jinja.builtins",
+        "django_jinja.management",
+        "django_jinja.management.commands",
+        "django_jinja.contrib",
+        "django_jinja.contrib._pipeline",
+        "django_jinja.contrib._pipeline.templatetags",
+        "django_jinja.contrib._easy_thumbnails",
+        "django_jinja.contrib._easy_thumbnails.templatetags",
+        "django_jinja.contrib._humanize",
+        "django_jinja.contrib._humanize.templatetags",
+        "django_jinja.contrib._subdomains",
+        "django_jinja.contrib._subdomains.templatetags",
+        "django_jinja.views",
+        "django_jinja.views.generic",
     ],
+    include_package_data = True,
+    package_data = {
+        "django_jinja": ["contrib/_pipeline/templates/pipeline/*.jinja"],
+    },
+    python_requires = ">=3.6",
+    install_requires = [
+        "jinja2>=3",
+        "django>=2.2",
+    ],
+    tests_require = [
+        "pytz",
+    ],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet :: WWW/HTTP",
+    ]
 )
