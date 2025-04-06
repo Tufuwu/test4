ebc69aff208fe2c008a12e35e6ce9fdd03bef1fc
wu <wu@.com>
2025-04-07 01:44:37 +0800

    2gis/k8s-handle/action

--- setup.py
+++ setup.py
@@ -1,38 +1,31 @@
-
 import os
-import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 
-version_py = os.path.join(os.path.dirname(__file__), 'gffutils', 'version.py')
-version = open(version_py).read().strip().split('=')[-1].replace('"', '')
-requirements = open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines()
-setup(
-    name='gffutils',
-    version=version,
-    install_requires=requirements,
-    packages=['gffutils', 'gffutils.scripts', 'gffutils.test',
-              'gffutils.test.data'],
-    scripts=['gffutils/scripts/gffutils-cli'],
-    author='Ryan Dale',
-    package_dir={'gffutils': 'gffutils'},
-    package_data = {'gffutils': ['test/data/*']},
-    description="Work with GFF and GTF files in a flexible "
-    "database framework",
-    author_email='dalerr@niddk.nih.gov',
-    url='https://github.com/daler/gffutils',
-    classifiers=[
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-)
+readme_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'README.md')
+
+
+def get_content(path):
+    with open(path, 'r') as f:
+        return f.read()
+
+
+setup(name='k8s-handle',
+      version=os.environ.get('RELEASE_TAG', '0.0.0'),
+      long_description=get_content(readme_path),
+      long_description_content_type='text/markdown',
+      description='Provisioning tool for Kubernetes apps',
+      url='http://github.com/2gis/k8s-handle',
+      author='Vadim Reyder',
+      author_email='vadim.reyder@gmail.com',
+      license='Apache 2.0',
+      packages=find_packages(exclude=("tests",)),
+      data_files=['requirements.txt'],
+      entry_points={
+          "console_scripts": [
+              "k8s-handle=k8s_handle:main",
+          ]
+      },
+      install_requires=get_content('requirements.txt').split('\n'),
+      zip_safe=False)
