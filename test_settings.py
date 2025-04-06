e92f8269d194c46b3e846aa3c20e2c5f8762664c
wu <wu@.com>
2025-04-07 00:37:46 +0800

    dppalomar/riskparity.py/action

--- test_settings.py
+++
@@ -1,8 +0,0 @@
-INSTALLED_APPS = (
-    'typedmodels',
-    'django.contrib.contenttypes',
-    'testapp',
-)
-MIDDLEWARE_CLASSES = ()
-DATABASES = {'default': {'NAME': ':memory:', 'ENGINE': 'django.db.backends.sqlite3'}}
-SECRET_KEY = 'abc123'
