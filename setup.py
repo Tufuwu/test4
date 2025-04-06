0087ca507d015a2907839a444c8b86251ee20b93
wu <wu@.com>
2025-04-06 20:44:40 +0800

    pympler/pympler/action

--- setup.py
+++ setup.py
@@ -1,52 +1,105 @@
-from setuptools import setup
-import codecs
+"""Setup script for Pympler.
+
+To build, install and test Pympler and to try Pympler
+before building and installing it.
+
+The HTML documentation is in the doc/ directory.  Point
+your browser to the ./doc/html/index.html file.
+
+"""
+import sys
+
+
+def _not_supported(why):
+    print('NotImplementedError: ' + why + '.')
+    sys.exit(1)
+
+
+if sys.hexversion < 0x3050000:
+    _not_supported('Pympler requires Python 3.5 or newer')
+
 import os
+from setuptools import Command
+from setuptools import setup
+from setuptools import Distribution
+from subprocess import run
 
 
-VERSION = '1.4.1'
-AUTHOR_NAME = 'Andy Port'
-AUTHOR_EMAIL = 'AndyAPort@gmail.com'
+class BaseTestCommand(Command):
+    """Base class for the pre and the post installation commands. """
+    user_options = []
+
+    def initialize_options(self):
+        self.param = None
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        args = [sys.executable,  # this Python binary
+                os.path.join('test', 'runtest.py'),
+                self.param, '-verbose', '3']
+        args.extend(sys.argv[2:])
+        sys.exit(run(args).returncode)
 
 
-def read(*parts):
-    """
-    Build an absolute path from *parts* and and return the contents of the
-    resulting file.  Assume UTF-8 encoding.
-    """
-    HERE = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
-        return f.read()
+class PreinstallTestCommand(BaseTestCommand):
+    description = "run pre-installation tests"
+
+    def initialize_options(self):
+        self.param = '-pre-install'
 
 
-setup(name='svgpathtools',
-      packages=['svgpathtools'],
-      version=VERSION,
-      description=('A collection of tools for manipulating and analyzing SVG '
-                   'Path objects and Bezier curves.'),
-      long_description=read("README.md"),
-      long_description_content_type='text/markdown',
-      author=AUTHOR_NAME,
-      author_email=AUTHOR_EMAIL,
-      url='https://github.com/mathandy/svgpathtools',
-      # download_url = 'http://github.com/mathandy/svgpathtools/tarball/'+VERSION,
-      license='MIT',
-      install_requires=['numpy', 'svgwrite'],
-      platforms="OS Independent",
-      requires=['numpy', 'svgwrite'],
-      keywords=['svg', 'svg path', 'svg.path', 'bezier', 'parse svg path', 'display svg'],
-      classifiers=[
-            "Development Status :: 4 - Beta",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
-            "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
-            "Topic :: Scientific/Engineering",
-            "Topic :: Scientific/Engineering :: Image Recognition",
-            "Topic :: Scientific/Engineering :: Information Analysis",
-            "Topic :: Scientific/Engineering :: Mathematics",
-            "Topic :: Scientific/Engineering :: Visualization",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-            ],
-      )
+class PostinstallTestCommand(BaseTestCommand):
+    description = "run post-installation tests"
+
+    def initialize_options(self):
+        self.param = '-post-install'
+
+
+def run_setup(include_tests=0):
+    tests = []
+    if include_tests:
+        tests = ['test', 'test.asizeof', 'test.tracker', 'test.muppy',
+                 'test.gui']
+
+    setup(packages=['pympler', 'pympler.util'] + tests,
+          package_data={'pympler': ['templates/*.html',
+                                    'templates/*.tpl',
+                                    'templates/*.js',
+                                    'templates/*.css',
+                                    'static/*.js']},
+          platforms=['any'],
+          classifiers=['Development Status :: 4 - Beta',
+                       'Environment :: Console',
+                       'Intended Audience :: Developers',
+                       'License :: OSI Approved :: Apache Software License',
+                       'Operating System :: OS Independent',
+                       'Programming Language :: Python',
+                       'Programming Language :: Python :: 3',
+                       'Programming Language :: Python :: 3.5',
+                       'Programming Language :: Python :: 3.6',
+                       'Programming Language :: Python :: 3.7',
+                       'Programming Language :: Python :: 3.8',
+                       'Programming Language :: Python :: 3.9',
+                       'Programming Language :: Python :: 3.10',
+                       'Topic :: Software Development :: Bug Tracking',
+                       ],
+          cmdclass={'try': PreinstallTestCommand,
+                    'test': PostinstallTestCommand,
+                    }
+          )
+
+
+try:  # hack Pympler commands into setup.py help output
+    Distribution.common_usage += """
+Pympler commands
+  setup.py try     try Pympler before installation
+  setup.py test    test Pympler after installation
+"""
+except AttributeError:
+    pass
+
+# Only include tests if creating a distribution package
+# (i.e. do not install the tests).
+run_setup('sdist' in sys.argv)
