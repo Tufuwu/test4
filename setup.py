33f508098e73e8ec63d6efee6c19370a3f40d981
wu <wu@.com>
2025-04-06 20:16:17 +0800

    amoffat/sh/action

--- setup.py
+++ setup.py
@@ -1,137 +1,59 @@
+from __future__ import print_function
 import os
+from os.path import dirname, abspath, join
 import sys
-from shutil import rmtree
-
-from setuptools import Command
-from setuptools import find_packages
+import sh
+import codecs
 from setuptools import setup
 
-# Package meta-data.
-NAME = 'django-inlinecss'
-SRC_DIR = 'django_inlinecss'
-DESCRIPTION = 'A Django app useful for inlining CSS (primarily for e-mails)'
-URL = 'https://github.com/roverdotcom/django-inlinecss'
-EMAIL = 'philip@rover.com'
-AUTHOR = 'Philip Kimmey'
-REQUIRES_PYTHON = '>=3.8'
-VERSION = None
 
-# What packages are required for this module to be executed?
-REQUIRED = [
-    'Django>=3.2',
-    'pynliner',
-    'future>=0.16.0',
-]
+HERE = dirname(abspath(__file__))
 
-# What packages are required only for tests?
-TESTS = [
-    'mock==5.1.0',
-    'pytest==8.0.1',
-    'pytest-django==4.8.0',
-]
-
-# What packages are optional?
-EXTRAS = {
-    'flake8': [
-        'flake8==7.0.0',
-        'flake8-isort==6.1.1',
-        'isort==5.13.2',
-        'testfixtures==8.0.0',
-    ],
-    "tests": TESTS,
-}
-
-here = os.path.abspath(os.path.dirname(__file__))
+author = "Andrew Moffat"
+author_email = "arwmoffat@gmail.com"
+keywords = ["subprocess", "process", "shell", "launch", "program"]
 
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-
-try:
-    with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-        long_description = "\n" + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    with open(os.path.join(here, SRC_DIR, "__version__.py")) as f:
-        exec(f.read(), about)
-else:
-    about["__version__"] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = "Build and publish the package."
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print(f"\033[1m{s}\033[0m")
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status("Removing previous builds…")
-            rmtree(os.path.join(here, "dist"))
-        except OSError:
-            pass
-
-        self.status("Building Source and Wheel (universal) distribution…")
-        os.system(f"{sys.executable} setup.py sdist bdist_wheel --universal")
-
-        self.status("Uploading the package to PyPI via Twine…")
-        os.system("twine upload dist/*")
-
-        self.status("Pushing git tags…")
-        os.system("git tag v{}".format(about["__version__"]))
-        os.system("git push --tags")
-
-        sys.exit()
-
+def read(*parts):
+    with codecs.open(join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
 
 setup(
-    name=NAME,
-    version=about["__version__"],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author=AUTHOR,
-    author_email=EMAIL,
+    name="sh",
+    version=sh.__version__,
+    description="Python subprocess replacement",
+    long_description=read("README.rst"),
+    author=author,
+    author_email=author_email,
+    maintainer=author,
+    maintainer_email=author_email,
+    keywords=keywords,
+    url="https://github.com/amoffat/sh",
     license="MIT",
-    url=URL,
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    keywords=["html", "css", "inline", "style", "email"],
+    py_modules=["sh"],
     classifiers=[
-        "Environment :: Other Environment",
-        "Environment :: Web Environment",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
         "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Communications :: Email",
-        "Topic :: Text Processing :: Markup :: HTML",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    install_requires=REQUIRED,
-    tests_require=TESTS,
-    extras_require=EXTRAS,
-    # $ setup.py upload support.
-    cmdclass={
-        "upload": UploadCommand,
-    },
 )
