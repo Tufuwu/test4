e892a767d2fa646ba495e85ad1828cc542e04001
wu <wu@.com>
2025-04-07 01:03:11 +0800

    chaoss/grimoirelab-graal/action

--- setup.py
+++ setup.py
@@ -1,83 +1,107 @@
-# This code is part of Qiskit
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 #
-# (C) Copyright IBM 2017.
+# Copyright (C) 2015-2020 Bitergia
 #
-# This code is licensed under the Apache License, Version 2.0. You may
-# obtain a copy of this license in the LICENSE.txt file in the root directory
-# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 #
-# Any modifications or derivative works of this code must retain this
-# copyright notice, and modified files need to carry a notice indicating
-# that they have been altered from the originals.
-
-# Copyright 2019-2020 Honeywell, Intl. (www.honeywell.com)
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# Authors:
+#     Valerio Cosentino <valcos@bitergia.com>
+#     inishchith <inishchith@gmail.com>
+#     Santiago Dueñas <sduenas@bitergia.com>
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-import setuptools
-import os
+import codecs
+import os.path
+import re
+import sys
+import pkg_resources
+import unittest
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup, Command
 
-requirements = [
-    'nest-asyncio>=1.2.0',
-    'qiskit-terra>=0.10',
-    'requests>=2.19',
-    'websockets>=7'
-]
+here = os.path.abspath(os.path.dirname(__file__))
+readme_md = os.path.join(here, 'README.md')
+version_py = os.path.join(here, 'graal', '_version.py')
 
-version_path = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), 'qiskit', 'providers', 'honeywell',
-    'VERSION.txt'))
+# Get the package description from the README.md file
+with codecs.open(readme_md, encoding='utf-8') as f:
+    long_description = f.read()
 
-with open(version_path, 'r') as fd:
-    version_str = fd.read().rstrip()
+with codecs.open(version_py, 'r', encoding='utf-8') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
 
-setuptools.setup(
-    name="qiskit-honeywell-provider",
-    version=version_str,
-    author="Honeywell",
-    author_email="dominic.lucchetti@honeywell.com",
-    license="Apache 2.0",
-    description="Qiskit provider for accessing the quantum devices at Honeywell",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url="https://github.com/qiskit-community/qiskit-honeywell-provider",
-    packages=setuptools.find_namespace_packages(include=['qiskit.*']),
-    install_requires=requirements,
-    python_requires=">=3.5",
-    include_package_data=True,
-    keywords="qiskit quantum",
-    project_urls={
-        "Bug Tracker": "https://github.com/qiskit-community/qiskit-honeywell-provider/issues",
-        "Documentation": "https://qiskit.org/documentation/",
-        "Source Code": "https://github.com/qiskit-community/qiskit-honeywell-provider"
-    },
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Operating System :: OS Independent",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: MacOS",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering"
-    ],
-    zip_safe=False,
-)
+
+class TestCommand(Command):
+
+    user_options = []
+    __dir__ = os.path.dirname(os.path.realpath(__file__))
+
+    def initialize_options(self):
+        os.chdir(os.path.join(self.__dir__, 'tests'))
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # To ensure it gets Perceval from the right directory, set first position of sys.path
+        sys.path.insert(0, pkg_resources.get_distribution("perceval").location)
+        test_suite = unittest.TestLoader().discover('.', pattern='test*.py')
+        result = unittest.TextTestRunner(buffer=True).run(test_suite)
+        sys.exit(not result.wasSuccessful())
+
+
+cmdclass = {'test': TestCommand}
+
+setup(name="graal",
+      description="A generic source code analyzer",
+      long_description="Graal extends the Git backend of Perceval to enable source code analysis. Thus, "
+                       "it fetches the commits from a Git repository and provides a mechanism to plug third party "
+                       "tools/libraries focused on source code analysis",
+      long_description_content_type='text/markdown',
+      url="https://github.com/chaoss/grimoirelab-graal",
+      version=version,
+      author="Bitergia",
+      author_email="valcos@bitergia.com",
+      license="GPLv3",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python :: 3'
+      ],
+      keywords="git source code analysis loc cyclomatic complexity",
+      packages=[
+          'graal',
+          'graal.backends',
+          'graal.backends.core',
+          'graal.backends.core.analyzers'
+      ],
+      namespace_packages=['graal', 'graal.backends'],
+      install_requires=[
+          'lizard==1.16.6',
+          'perceval>=0.12.0',
+          'pylint>=1.8.4',
+          'flake8>=3.7.7',
+          'networkx>=2.1',
+          'pydot>=1.2.4',
+          'bandit>=1.4.0'
+      ],
+      scripts=[
+          'bin/graal'
+      ],
+      cmdclass=cmdclass,
+      zip_safe=False)
