c2430a0e114d1b8df59473feeaf117aa295a2f63
wu <wu@.com>
2025-04-06 21:06:00 +0800

    bpython/curtsies/action

--- setup.py
+++ setup.py
@@ -1,32 +1,49 @@
-#!/usr/bin/env python
-
+from setuptools import setup
+import ast
 import os
-from pathlib import Path
-import setuptools
-import versioneer
+import io
 
-install_requires = [
-    line.rstrip() for line in open(Path(__file__).parent / "REQUIREMENTS.txt")
-]
 
-with open(Path("starfish") / "core" / "_display.py") as f:
-    for line in f.readlines():
-        if line.startswith("NAPARI_VERSION"):
-            napari_version = line.split('"')[1]
-            break
+def version():
+    """Return version string."""
+    with open(os.path.join("curtsies", "__init__.py")) as input_file:
+        for line in input_file:
+            if line.startswith("__version__"):
+                return ast.parse(line).body[0].value.s
 
-setuptools.setup(
-    packages=setuptools.find_packages(),
-    install_requires=install_requires,
-    extras_require={
-        'napari': [f"napari[all]>={napari_version}"],
-    },
-    entry_points={
-        'console_scripts': [
-            "starfish=starfish.core.starfish:starfish",
-        ]
-    },
-    include_package_data=True,
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+
+def long_description():
+    with open("readme.md", encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name="curtsies",
+    version=version(),
+    description="Curses-like terminal wrapper, with colored strings!",
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bpython/curtsies",
+    author="Thomas Ballinger",
+    author_email="thomasballinger@gmail.com",
+    license="MIT",
+    packages=["curtsies"],
+    install_requires=[
+        "blessings>=1.5",
+        "wcwidth>=0.1.4",
+    ],
+    tests_require=[
+        "pyte",
+        "pytest",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    zip_safe=False,
 )
