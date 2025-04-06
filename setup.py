817a608eb52eb2904a60f37013d4ef33d587f01b
wu <wu@.com>
2025-04-07 00:35:07 +0800

    eugene-eeo/tinyrecord/action

--- setup.py
+++ setup.py
@@ -1,52 +1,39 @@
 import os
+from setuptools import setup
 
-from setuptools import find_packages, setup
+classifiers = """\
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3 :: Only
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: Microsoft :: Windows
+Operating System :: Unix
+Operating System :: MacOS :: MacOS X
+"""
 
-with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
-    README = readme.read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
+curr_path = os.path.abspath(os.path.dirname(__file__))
 setup(
-    name="django-phone-verify",
-    version="2.0.1",
-    packages=find_packages(),
-    include_package_data=True,
-    license="GPLv3",
-    description="A Django app to support phone number verification using security code sent via SMS.",
-    long_description=README,
-    url="https://github.com/CuriousLearner/django-phone-verify",
-    author="Sanyam Khurana",
-    author_email="sanyam@sanyamkhurana.com",
-    install_requires=[
-        "django>=2.1.5",
-        "djangorestframework>=3.9.0",
-        "PyJWT>=1.7.1",
-        "python-dotenv>=0.10.0",
-        "phonenumbers>=8.10.2",
-        "django-phonenumber-field>=2.1.0",
-        "twilio>=6.21.0",
-        "nexmo>=2.4.0",
-    ],
-    classifiers=[
-        "Environment :: Web Environment",
-        "Development Status :: 5 - Production/Stable",
-        "Framework :: Django",
-        "Framework :: Django :: 2.0",
-        "Framework :: Django :: 2.1",
-        "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.0",
-        "Framework :: Django :: 3.1",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-    ],
+    name='tinyrecord',
+    version='0.2.1',
+    packages=['tinyrecord'],
+    package_data={'tinyrecord': ['py.typed']},
+    python_requires='>=3.6',
+    install_requires=['tinydb >= 4.0.0'],
+    classifiers=filter(None, classifiers.split('\n')),
+    zip_safe=True,
+    author='Eugene Eeo',
+    author_email='141bytes@gmail.com',
+    long_description=open(os.path.join(curr_path, 'README.rst'), 'r').read(),
+    long_description_content_type='text/x-rst',
+    description='Atomic transactions for TinyDB',
+    license='MIT',
+    keywords='tinydb nosql database transaction',
+    url='https://github.com/eugene-eeo/tinyrecord',
 )
