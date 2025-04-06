6c4298c19c5b0da0c21bb629fdb18f2bf897ec4b
wu <wu@.com>
2025-04-06 23:42:32 +0800

    django-tastypie/django-tastypie/action

--- setup.py
+++ setup.py
@@ -1,69 +1,57 @@
-# Copyright 2020 The Magenta Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
-"""A setuptools based setup module for note-seq."""
 
-from setuptools import find_packages
-from setuptools import setup
+from tastypie import __version__
 
-with open('note_seq/version.py') as in_file:
-  exec(in_file.read())  # pylint: disable=exec-used
-
-REQUIRED_PACKAGES = [
-    'absl-py',
-    'attrs',
-    'bokeh >= 0.12.0',
-    'intervaltree >= 2.1.0',
-    'IPython',
-    'librosa >= 0.6.2',
-    'numba == 0.48.0',  # temporary fix for librosa import
-    'numpy',
-    'pandas >= 0.18.1',
-    'pretty_midi >= 0.2.6',
-    'protobuf >= 3.6.1',
-    'scikit-image',
-    'scipy >= 0.18.1',
-]
 
 setup(
-    name='note-seq',
-    version=__version__,  # pylint: disable=undefined-variable
-    description='Use machine learning to create art and music',
-    long_description='',
-    url='https://magenta.tensorflow.org/',
-    author='Google Inc.',
-    author_email='magenta-discuss@gmail.com',
-    license='Apache 2',
-    # PyPI package information.
+    name='django-tastypie',
+    version=__version__,
+    description='A flexible & capable API layer for Django.',
+    author='Daniel Lindsley',
+    author_email='daniel@toastdriven.com',
+    url='https://github.com/django-tastypie/django-tastypie',
+    long_description=open('README.rst', 'r').read(),
+    packages=[
+        'tastypie',
+        'tastypie.utils',
+        'tastypie.management',
+        'tastypie.management.commands',
+        'tastypie.migrations',
+        'tastypie.contrib',
+        'tastypie.contrib.gis',
+        'tastypie.contrib.contenttypes',
+    ],
+    package_data={
+        'tastypie': ['templates/tastypie/*'],
+    },
+    zip_safe=False,
+    requires=[
+        'python_mimeparse(>=0.1.4, !=1.5)',
+        'dateutil(>=1.5, !=2.0)',
+    ],
+    install_requires=[
+        'python-mimeparse >= 0.1.4, != 1.5',
+        'python-dateutil >= 1.5, != 2.0',
+    ],
+    tests_require=['mock', 'PyYAML', 'lxml', 'defusedxml'],
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Software Development :: Libraries',
-    ],
-    keywords='note_seq note sequences',
-    packages=find_packages(),
-    install_requires=REQUIRED_PACKAGES,
-    setup_requires=['pytest-runner', 'pytest-pylint'],
-    tests_require=[
-        'pytest >= 5.2.0',
-        'pytest-xdist < 1.30.0',  # 1.30 has problems working with pylint plugin
-        'pylint >= 2.4.2',
+        'Topic :: Utilities'
     ],
 )
