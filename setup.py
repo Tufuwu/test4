f7f1168a10451f392d9e5a6e2e659f63c098b471
wu <wu@.com>
2025-04-06 23:55:25 +0800

    joalla/discogs_client/action

--- setup.py
+++ setup.py
@@ -1,80 +1,31 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-from __future__ import print_function
 from setuptools import setup
-import re
-import os
-import sys
-
-
-name = "django-zen-queries"
-package = "zen_queries"
-description = "Explicit control over query execution in Django applications."
-url = "https://github.com/dabapps/django-zen-queries"
-author = "DabApps"
-author_email = "hello@dabapps.com"
-license = "BSD"
-
-long_description = description + "\n\nFor full details, see https://github.com/dabapps/django-zen-queries"
-
-
-def get_version(package):
-    """
-    Return package version as listed in `__version__` in `init.py`.
-    """
-    init_py = open(os.path.join(package, "__init__.py")).read()
-    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(
-        1
-    )
-
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [
-        dirpath
-        for dirpath, dirnames, filenames in os.walk(package)
-        if os.path.exists(os.path.join(dirpath, "__init__.py"))
-    ]
-
-
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [
-        (dirpath.replace(package + os.sep, "", 1), filenames)
-        for dirpath, dirnames, filenames in os.walk(package)
-        if not os.path.exists(os.path.join(dirpath, "__init__.py"))
-    ]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename) for filename in filenames])
-    return {package: filepaths}
-
-
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist upload")
-    args = {"version": get_version(package)}
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %(version)s -m 'version %(version)s'" % args)
-    print("  git push --tags")
-    sys.exit()
-
 
 setup(
-    name=name,
-    version=get_version(package),
-    url=url,
-    license=license,
-    description=description,
-    long_description=long_description,
-    author=author,
-    author_email=author_email,
-    packages=get_packages(package),
-    package_data=get_package_data(package),
-)
+        name='python3-discogs-client',
+        version='2.3.5',
+        description='Python API client for Discogs',
+        long_description='This is an active fork of the official "Discogs API client for Python", which was deprecated by discogs.com as of June 2020. We think it is a very useful Python module and decided to continue maintaining it. Please visit: https://github.com/joalla/discogs_client for more information.',
+        url='https://github.com/joalla/discogs_client',
+        author='joalla',
+        author_email='jt@peek-a-boo.at',
+        test_suite='discogs_client.tests',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'License :: OSI Approved :: BSD License',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Communications',
+            'Topic :: Utilities',
+            ],
+        install_requires=[
+            'requests',
+            'oauthlib',
+            ],
+        packages=[
+            'discogs_client',
+            ],
+        )
