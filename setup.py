9305e2e53d22248f0b67cfea3db6fdd6791a87f1
wu <wu@.com>
2025-04-06 23:21:26 +0800

    torchbox/wagtail-markdown/action

--- setup.py
+++ setup.py
@@ -1,27 +1,78 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import sys
-import os
-from setuptools import setup
-from pathlib import Path
-this_dir = Path(__file__).absolute().parent
+#! /usr/bin/env/python
+# vim:sw=4 ts=4 et:
+#
+# Copyright (c) 2015 Torchbox Ltd.
+# felicity@torchbox.com 2015-09-14
+#
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely. This software is provided 'as-is', without any express or implied
+# warranty.
+#
 
-if sys.argv[-1].startswith('publish'):
-    if os.system("pip list | grep wheel"):
-        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
-        sys.exit()
-    if os.system("pip list | grep twine"):
-        print("twine not installed.\nUse `pip install twine`.\nExiting.")
-        sys.exit()
-    os.system("python setup.py sdist bdist_wheel")
-    if sys.argv[-1] == 'publishtest':
-        os.system("twine upload -r test dist/*")
+import subprocess
+
+from setuptools import find_packages, setup
+
+
+def get_git_revision_hash():
+    try:
+        git_hash = subprocess.check_output(["git", "rev-parse", "HEAD"])
+    except subprocess.CalledProcessError:
+        return 'main'
     else:
-        os.system("twine upload dist/*")
-    sys.exit()
+        return git_hash.decode("ascii").splitlines()[0]
 
-if __name__ == "__main__":
-    setup(use_scm_version={
-        "write_to": str(this_dir / "parglare" / "version.py"),
-        "write_to_template": '__version__ = "{version}"\n',
-    })
+
+README = "https://github.com/torchbox/wagtail-markdown/blob/{hash}/README.md"
+README = README.format(hash=get_git_revision_hash())
+
+
+INSTALL_REQUIRES = [
+    'Markdown>=3,<4',
+    'bleach>=3.3.0,<4',
+    'Wagtail>=2.0',
+]
+
+
+TESTING_REQUIRES = [
+    "dj_database_url==0.5.0",
+]
+
+
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: zlib/libpng License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Framework :: Django",
+    "Framework :: Django :: 2.2",
+    "Framework :: Django :: 3.0",
+    "Framework :: Django :: 3.1",
+    "Framework :: Wagtail",
+    "Framework :: Wagtail :: 2",
+]
+
+
+setup(
+    name="wagtail-markdown",
+    version="0.7.0-alpha",
+    description="Markdown support for Wagtail",
+    long_description="Provides Markdown page field and streamfield block for "
+    "Wagtail. More info: {}".format(README),
+    author="Felicity Tarnell",
+    author_email="felicity@torchbox.com",
+    url="https://github.com/torchbox/wagtail-markdown",
+    project_urls={
+        "Changelog": "https://github.com/torchbox/wagtail-markdown/blob/main/CHANGELOG.md",
+    },
+    install_requires=INSTALL_REQUIRES,
+    license="zlib",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=CLASSIFIERS,
+    extras_require={"testing": TESTING_REQUIRES},
+)
--- tests/perf/runall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python test_cpu.py
-python test_mem.py
