eb8baf09b79ddd1da0786fa6cad575dccc76d367
wu <wu@.com>
2025-04-07 03:13:14 +0800

    dpgaspar/flask-appbuilder/action

--- setup.py
+++ setup.py
@@ -1,71 +1,83 @@
-#!/usr/bin/env python3
+import io
+import os
+import re
 
-# Copyright 2020-2021 Canonical Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# For further info, check https://github.com/canonical/charmcraft
-
-"""Setup script for Charmcraft."""
-
-from pathlib import Path
-from textwrap import dedent
-from setuptools import setup
-
-import charmcraft
+from setuptools import find_packages, setup
 
 
-with open("README.md", "rt", encoding='utf8') as fh:
-    long_description = fh.read()
+with io.open("flask_appbuilder/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r"__version__ = \"(.*?)\"", f.read()).group(1)
 
-with open("requirements.txt", "rt", encoding='utf8') as fh:
-    requirements = fh.read().split('\n')
 
-version_path = Path("charmcraft/version.py")
-version_backup = Path("charmcraft/version.py~")
-version_path.rename(version_backup)
-try:
-    with version_path.open("wt", encoding="utf8") as fh:
-        fh.write(dedent('''
-            # this is a generated file
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
 
-            version = {!r}
-            ''').format(charmcraft.__version__))
 
-    setup(
-        name="charmcraft",
-        version=charmcraft.__version__,
-        author="Facundo Batista",
-        author_email="facundo.batista@canonical.com",
-        description="The main tool to build, upload, and develop in general the Juju charms.",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        url="https://github.com/canonical/charmcraft",
-        license="Apache-2.0",
-        packages=['charmcraft', 'charmcraft.commands', 'charmcraft.commands.store'],
-        classifiers=[
-            "Environment :: Console",
-            "License :: OSI Approved :: Apache Software License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 3",
-        ],
-        entry_points={
-            'console_scripts': ["charmcraft = charmcraft.main:main"],
-        },
-        python_requires='>=3',
-        install_requires=requirements,
-        include_package_data=True,  # so we get templates in the wheel
-    )
+def read(fname):
+    return open(fpath(fname)).read()
 
-finally:
-    version_backup.rename(version_path)
+
+def desc():
+    return read("README.rst")
+
+
+setup(
+    name="Flask-AppBuilder",
+    version=version,
+    url="https://github.com/dpgaspar/flask-appbuilder/",
+    license="BSD",
+    author="Daniel Vaz Gaspar",
+    author_email="danielvazgaspar@gmail.com",
+    description=(
+        "Simple and rapid application development framework, built on top of Flask."
+        " includes detailed security, auto CRUD generation for your models,"
+        " google charts and much more."
+    ),
+    long_description=desc(),
+    long_description_content_type="text/x-rst",
+    packages=find_packages(),
+    package_data={"": ["LICENSE"]},
+    entry_points={
+        "flask.commands": ["fab=flask_appbuilder.cli:fab"],
+        "console_scripts": ["fabmanager = flask_appbuilder.console:cli"],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    platforms="any",
+    install_requires=[
+        "apispec[yaml]>=3.3, <4",
+        "colorama>=0.3.9, <1",
+        "click>=6.7, <8",
+        "email_validator>=1.0.5, <2",
+        "Flask>=0.12, <2",
+        "Flask-Babel>=1, <2",
+        "Flask-Login>=0.3, <0.5",
+        "Flask-OpenID>=1.2.5, <2",
+        "Flask-SQLAlchemy>=2.4, <3",
+        "Flask-WTF>=0.14.2, <1",
+        "Flask-JWT-Extended>=3.18, <4",
+        "jsonschema>=3.0.1, <4",
+        "marshmallow>=3, <4",
+        "marshmallow-enum>=1.5.1, <2",
+        "marshmallow-sqlalchemy>=0.22.0, <1",
+        "python-dateutil>=2.3, <3",
+        "prison>=0.1.3, <1.0.0",
+        "PyJWT>=1.7.1",
+        "sqlalchemy-utils>=0.32.21, <1",
+    ],
+    extras_require={"jmespath": ["jmespath>=0.9.5"]},
+    tests_require=["nose>=1.0", "mockldap>=0.3.0"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires="~=3.6",
+    test_suite="nose.collector",
+)
