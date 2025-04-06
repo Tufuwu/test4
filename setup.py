97e83dc6f505017ff8af2f677f3474ae2766b566
wu <wu@.com>
2025-04-07 00:06:13 +0800

    kipe/pycron/action

--- setup.py
+++ setup.py
@@ -1,65 +1,27 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-"""Setup for PyPI usage."""
-
-import os.path as op
-from glob import glob
 from setuptools import setup
-from setup_helper import SetupHelper
-
-import versioneer
-
-NAME = "goatools"
-CLASSIFIERS = [
-    "Development Status :: 4 - Beta",
-    "Intended Audience :: Science/Research",
-    "License :: OSI Approved :: BSD License",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 3",
-    "Topic :: Scientific/Engineering :: Bio-Informatics",
-]
-
-# Use the helper
-HLPR = SetupHelper(initfile="goatools/__init__.py", readmefile="README.md")
-
-SETUP_DIR = op.abspath(op.dirname(__file__))
-REQUIREMENTS = [
-    x.strip() for x in open(op.join(SETUP_DIR, "requirements.txt")).readlines()
-]
 
 setup(
-    name=NAME,
-    version=versioneer.get_version(),
-    author=HLPR.author,
-    author_email=HLPR.email,
-    license=HLPR.license,
-    long_description=HLPR.long_description,
-    long_description_content_type="text/markdown",
-    cmdclass=versioneer.get_cmdclass(),
+    name='pycron',
+    version='3.0.0',
+    description='Simple cron-like parser, which determines if current datetime matches conditions.',
+    author='Kimmo Huoman',
+    author_email='kipenroskaposti@gmail.com',
+    license='MIT',
+    keywords='cron parser',
+    url='https://github.com/kipe/pycron',
     packages=[
-        NAME,
-        NAME + ".godag",
-        NAME + ".gosubdag",
-        NAME + ".gosubdag.plot",
-        NAME + ".gosubdag.rpt",
-        NAME + ".test_data",
-        NAME + ".test_data.sections",
-        NAME + ".test_data.cli",
-        NAME + ".cli",
-        NAME + ".rpt",
-        NAME + ".anno",
-        NAME + ".anno.init",
-        NAME + ".anno.extensions",
-        NAME + ".goea",
-        NAME + ".grouper",
-        NAME + ".parsers",
+        'pycron',
     ],
-    include_package_data=True,
-    package_data={"goatools.test_data.nbt_3102": ["*.*"]},
-    scripts=glob("scripts/*.py"),
-    classifiers=CLASSIFIERS,
-    url="http://github.com/tanghaibao/goatools",
-    description="Python scripts to find enrichment of GO terms",
-    install_requires=REQUIREMENTS,
+    python_requires='>=3.5',
+    tests_require=[
+        "arrow>=0.12.0",
+        "coverage>=4.4.2",
+        "coveralls>=1.2.0",
+        "Delorean>=0.6.0",
+        "nose>=1.0",
+        "pendulum>=1.3.2",
+        "pytz>=2017.3",
+        "udatetime>=0.0.14"
+    ]
 )
