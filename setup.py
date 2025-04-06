d939b6bea83a52df332d898eed80cfb8a41ef4b6
wu <wu@.com>
2025-04-06 23:40:17 +0800

    webrecorder/warcio/action

--- setup.py
+++ setup.py
@@ -1,62 +1,70 @@
 #!/usr/bin/env python
-import sys
-from distutils.core import Command
-import subprocess
+# vim: set sw=4 et:
 
-from setuptools import setup
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import glob
 
-import defusedxml
+__version__ = '1.7.4'
 
 
-class PyTest(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
+class PyTest(TestCommand):
     def finalize_options(self):
-        pass
+        TestCommand.finalize_options(self)
+        # should work with setuptools <18, 18 18.5
+        self.test_suite = ' '
 
-    def run(self):
-        errno = subprocess.call([sys.executable, "tests.py"])
-        raise SystemExit(errno)
-
-
-long_description = []
-with open("README.txt") as f:
-    long_description.append(f.read())
-with open("CHANGES.txt") as f:
-    long_description.append(f.read())
-
+    def run_tests(self):
+        import pytest
+        import sys
+        import os
+        errcode = pytest.main(['--doctest-modules', './warcio', '--cov', 'warcio', '-v', 'test/'])
+        sys.exit(errcode)
 
 setup(
-    name="defusedxml",
-    version=defusedxml.__version__,
-    cmdclass={"test": PyTest},
-    packages=["defusedxml"],
-    author="Christian Heimes",
-    author_email="christian@python.org",
-    maintainer="Christian Heimes",
-    maintainer_email="christian@python.org",
-    url="https://github.com/tiran/defusedxml",
-    download_url="https://pypi.python.org/pypi/defusedxml",
-    keywords="xml bomb DoS",
-    platforms="all",
-    license="PSFL",
-    description="XML bomb protection for Python stdlib modules",
-    long_description="\n".join(long_description),
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Python Software Foundation License",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Text Processing :: Markup :: XML",
+    name='warcio',
+    version=__version__,
+    author='Ilya Kreymer',
+    author_email='ikreymer@gmail.com',
+    license='Apache 2.0',
+    packages=find_packages(exclude=['test']),
+    url='https://github.com/webrecorder/warcio',
+    description='Streaming WARC (and ARC) IO library',
+    long_description=open('README.rst').read(),
+    provides=[
+        'warcio',
+        ],
+    install_requires=[
+        'six',
+        ],
+    zip_safe=True,
+    entry_points="""
+        [console_scripts]
+        warcio = warcio.cli:main
+    """,
+    cmdclass={'test': PyTest},
+    test_suite='',
+    tests_require=[
+        'urllib3==1.25.11',
+        'pytest',
+        'pytest-cov',
+        'httpbin>=0.10.2',
+        'requests',
+        'wsgiprox',
+        'hookdns',
     ],
-    python_requires=">=3.6",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ]
 )
