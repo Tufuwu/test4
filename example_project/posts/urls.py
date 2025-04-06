14e6e701aec75a7bc6c09add741cc6e7aba649e9
wu <wu@.com>
2025-04-07 00:10:00 +0800

    biocore/scikit-bio/action

--- example_project/posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from posts import views
-
-urlpatterns = [
-    path('', views.post_list, name='posts_post_list'),
-    path('<slug:slug>', views.post_detail, name='posts_post_detail'),
-]
