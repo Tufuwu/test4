0a4637e25d7651870ae51bff0752f09b709a6e14
wu <wu@.com>
2025-04-07 02:25:15 +0800

    litex-hub/litespi/action

--- setup.py
+++ setup.py
@@ -1,46 +1,18 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python3
 
-with open('README.md', encoding='utf-8') as f:
-    readme = f.read()
+from setuptools import setup
+from setuptools import find_packages
 
 
 setup(
-    name='btrccts',
-    version='0.1.1',
-    description='BackTest and Run CryptoCurrency Trading Strategies',
-    long_description=readme,
-    long_description_content_type='text/markdown',
-    classifiers=[
-        'Programming Language :: Python',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Financial and Insurance Industry',
-        'Topic :: Software Development :: Build Tools',
-        'Topic :: Office/Business :: Financial :: Investment',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Operating System :: OS Independent',
-        'Environment :: Console'
-    ],
-    author='Simon Brand',
-    author_email='simon.brand@postadigitale.de',
-    url='https://github.com/btrccts/btrccts/',
-    keywords='btrccts',
-    package_dir={'': 'src'},
-    packages=find_packages('src/'),
+    name="litespi",
+    description="Small footprint and configurable SPI core",
+    author="LiteSPI Developers",
+    url="https://github.com/litex-hub",
+    download_url="https://github.com/litex-hub/litespi",
+    test_suite="test",
+    license="BSD",
+    python_requires="~=3.6",
+    packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
     include_package_data=True,
-    zip_safe=False,
-    extras_require={
-        'dev': ['pycodestyle', 'pyflakes'],
-        'ccxt-websockets': ['ccxtpro'],
-    },
-    install_requires=['ccxt', 'pandas', 'numpy', 'appdirs'],
-    entry_points={
-        'console_scripts': [
-            'btrccts=btrccts:_main',
-        ]
-    },
 )
