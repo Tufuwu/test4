e78033220753578c4606023fcc2be921ca955aac
wu <wu@.com>
2025-04-07 02:36:09 +0800

    cuenca-mx/clabe-python/action

--- setup.py
+++ setup.py
@@ -1,114 +1,43 @@
-# -*- coding: utf-8 -*-
-# ----------------------------------------------------------------------------
-# Copyright (C) British Crown (Met Office) & Contributors.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# ----------------------------------------------------------------------------
+from importlib.machinery import SourceFileLoader
 
-from importlib.util import spec_from_file_location, module_from_spec
-from pathlib import Path
-# overriding setuptools command
-# https://stackoverflow.com/a/51294311
-from setuptools.command.bdist_rpm import bdist_rpm as bdist_rpm_original
-# to parse pytest command arguments
-# https://docs.pytest.org/en/2.8.7/goodpractices.html#manual-integration
-from setuptools import setup, find_namespace_packages
+import setuptools
+
+version = SourceFileLoader('version', 'clabe/version.py').load_module()
 
 
-DIST_DIR = Path(__file__).resolve().parent
-
-
-def get_version(module, path):
-    """Return the __version__ attr from a module sourced by FS path."""
-    spec = spec_from_file_location(module, path)
-    module = module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module.__version__
-
-
-__version__ = get_version(
-    'metomi.isodatetime',
-    str(Path(DIST_DIR, 'metomi/isodatetime/__init__.py'))
-)
-
-
-class bdist_rpm(bdist_rpm_original):
-
-    def run(self):
-        """Before calling the original run method, let's change the
-        distribution name to create an RPM for python-isodatetime."""
-        self.distribution.metadata.name = "python-isodatetime"
-        self.distribution.metadata.version = __version__
-        super().run()
-
-
-install_requires = []
-tests_require = [
-    'coverage',
-    'pytest>=5',
-    'pytest-env',
+test_requires = [
+    'pytest<5.2',
+    'coverage<5',
     'pytest-cov',
-    'flake8'
+    'black',
+    'isort[pipfile]',
+    'flake8',
+    'mypy',
 ]
-extras_require = {}
-extras_require['all'] = (
-    tests_require
-    + list({
-        req
-        for reqs in extras_require.values()
-        for req in reqs
-    })
-)
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
 
-setup(
-    name="metomi-isodatetime",
-    version='1!' + __version__,
-    author="Met Office",
-    author_email="metomi@metoffice.gov.uk",
-    cmdclass={
-        "bdist_rpm": bdist_rpm
-    },
-    description=("Python ISO 8601 date time parser" +
-                 " and data model/manipulation utilities"),
-    license="LGPLv3",
-    keywords="isodatetime datetime iso8601 date time parser",
-    url="https://github.com/metomi/isodatetime",
-    packages=find_namespace_packages(include=['metomi.*']),
-    long_description=open(str(Path(DIST_DIR, 'README.md')), 'r').read(),
-    long_description_content_type="text/markdown",
-    platforms='any',
-    install_requires=install_requires,
-    tests_require=tests_require,
-    extras_require=extras_require,
-    python_requires='>=3.5',
+setuptools.setup(
+    name='clabe',
+    version=version.__version__,
+    author='Cuenca',
+    author_email='dev@cuenca.com',
+    description='Validate and generate the control digit of a CLABE in Mexico',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cuenca-mx/clabe',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data=dict(mati=['py.typed']),
+    install_requires=['pydantic>=1.4,<2.0'],
+    setup_requires=['pytest-runner'],
+    tests_require=test_requires,
+    extras_require=dict(test=test_requires),
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        ("License :: OSI Approved" +
-         " :: GNU Lesser General Public License v3 (LGPLv3)"),
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Utilities"
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
-    entry_points={
-        'console_scripts': ['isodatetime=metomi.isodatetime.main:main'],
-    },
 )
