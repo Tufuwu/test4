d830b43cde78e9cd04fdcb1da2868a141df08ddd
wu <wu@.com>
2025-04-07 00:04:59 +0800

    tanghaibao/goatools/action

--- setup.py
+++ setup.py
@@ -1,93 +1,65 @@
-import re
-from codecs import open  # to use a consistent encoding
-from collections import OrderedDict
-from os import path
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Setup for PyPI usage."""
+
+import os.path as op
+from glob import glob
 from setuptools import setup
+from setup_helper import SetupHelper
 
-here = path.abspath(path.dirname(__file__))
+import versioneer
 
-# get versions from anymail/_version.py,
-# but without importing from anymail (which would break setup)
-with open(path.join(here, "anymail/_version.py"), encoding='utf-8') as f:
-    code = compile(f.read(), "anymail/_version.py", 'exec')
-    _version = {}
-    exec(code, _version)
-    version = _version["__version__"]  # X.Y or X.Y.Z or X.Y.Z.dev1 etc.
-    release_tag = "v%s" % version  # vX.Y or vX.Y.Z
+NAME = "goatools"
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+]
 
+# Use the helper
+HLPR = SetupHelper(initfile="goatools/__init__.py", readmefile="README.md")
 
-def long_description_from_readme(rst):
-    # Freeze external links (on PyPI) to refer to this X.Y or X.Y.Z tag.
-    # (This relies on tagging releases with 'vX.Y' or 'vX.Y.Z' in GitHub.)
-    rst = re.sub(r'(?<=branch[=:])main'    # GitHub Actions build status: branch=main --> branch=vX.Y.Z
-                 r'|(?<=/)stable'          # ReadTheDocs links: /stable --> /vX.Y.Z
-                 r'|(?<=version=)stable',  # ReadTheDocs badge: version=stable --> version=vX.Y.Z
-                 release_tag, rst)  # (?<=...) is "positive lookbehind": must be there, but won't get replaced
-    return rst
-
-
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = long_description_from_readme(f.read())
-
+SETUP_DIR = op.abspath(op.dirname(__file__))
+REQUIREMENTS = [
+    x.strip() for x in open(op.join(SETUP_DIR, "requirements.txt")).readlines()
+]
 
 setup(
-    name="django-anymail",
-    version=version,
-    description='Django email integration for Amazon SES, Mailgun, Mailjet, Postmark, '
-                'SendGrid, SendinBlue, SparkPost and other transactional ESPs',
-    keywords="Django, email, email backend, ESP, transactional mail, "
-             "Amazon SES, Mailgun, Mailjet, Mandrill, Postmark, SendinBlue, SendGrid, SparkPost",
-    author="Mike Edmunds and Anymail contributors",
-    author_email="medmunds@gmail.com",
-    url="https://github.com/anymail/django-anymail",
-    license="BSD License",
-    packages=["anymail"],
-    zip_safe=False,
-    python_requires='>=3.5',
-    install_requires=["django>=2.0", "requests>=2.4.3"],
-    extras_require={
-        # This can be used if particular backends have unique dependencies.
-        # For simplicity, requests is included in the base requirements.
-        "amazon_ses": ["boto3"],
-        "mailgun": [],
-        "mailjet": [],
-        "mandrill": [],
-        "postmark": [],
-        "sendgrid": [],
-        "sendinblue": [],
-        "sparkpost": [],
-    },
-    include_package_data=True,
-    test_suite="runtests.runtests",
-    tests_require=["mock", "boto3"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: BSD License",
-        "Topic :: Communications :: Email",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Intended Audience :: Developers",
-        "Framework :: Django",
-        "Framework :: Django :: 2.0",
-        "Framework :: Django :: 2.1",
-        "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.0",
-        "Framework :: Django :: 3.1",
-        "Environment :: Web Environment",
+    name=NAME,
+    version=versioneer.get_version(),
+    author=HLPR.author,
+    author_email=HLPR.email,
+    license=HLPR.license,
+    long_description=HLPR.long_description,
+    long_description_content_type="text/markdown",
+    cmdclass=versioneer.get_cmdclass(),
+    packages=[
+        NAME,
+        NAME + ".godag",
+        NAME + ".gosubdag",
+        NAME + ".gosubdag.plot",
+        NAME + ".gosubdag.rpt",
+        NAME + ".test_data",
+        NAME + ".test_data.sections",
+        NAME + ".test_data.cli",
+        NAME + ".cli",
+        NAME + ".rpt",
+        NAME + ".anno",
+        NAME + ".anno.init",
+        NAME + ".anno.extensions",
+        NAME + ".goea",
+        NAME + ".grouper",
+        NAME + ".parsers",
     ],
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    project_urls=OrderedDict([
-        ("Documentation", "https://anymail.readthedocs.io/en/%s/" % release_tag),
-        ("Source", "https://github.com/anymail/django-anymail"),
-        ("Changelog", "https://anymail.readthedocs.io/en/%s/changelog/" % release_tag),
-        ("Tracker", "https://github.com/anymail/django-anymail/issues"),
-    ]),
+    include_package_data=True,
+    package_data={"goatools.test_data.nbt_3102": ["*.*"]},
+    scripts=glob("scripts/*.py"),
+    classifiers=CLASSIFIERS,
+    url="http://github.com/tanghaibao/goatools",
+    description="Python scripts to find enrichment of GO terms",
+    install_requires=REQUIREMENTS,
 )
