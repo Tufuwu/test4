dfdca88a3e321075f63a2b58db76c0c67d2c6436
wu <wu@.com>
2025-04-07 02:43:11 +0800

    tenable/pytenable/action

--- setup.py
+++ setup.py
@@ -1,48 +1,57 @@
-#!/usr/bin/python2
-
-from distutils.core import setup
+from setuptools import setup, find_packages
 import os
-import sys
 
+with open('tenable/version.py', 'r') as vfile:
+    exec(vfile.read())
 
-# config file
-data_files = [("/etc/lorax", ["etc/lorax.conf"]),
-              ("/usr/lib/tmpfiles.d/", ["systemd/lorax.conf"])]
-
-# shared files
-for root, dnames, fnames in os.walk("share"):
-    for fname in fnames:
-        data_files.append((root.replace("share", "/usr/share/lorax", 1),
-                           [os.path.join(root, fname)]))
-
-# executable
-data_files.append(("/usr/sbin", ["src/sbin/lorax", "src/sbin/mkefiboot",
-                                 "src/sbin/livemedia-creator", "src/sbin/mkksiso"]))
-data_files.append(("/usr/bin",  ["src/bin/image-minimizer",
-                                 "src/bin/composer-cli"]))
-
-# get the version
-sys.path.insert(0, "src")
 try:
-    import pylorax.version
-except ImportError:
-    vernum = "devel"
-else:
-    vernum = pylorax.version.num
-finally:
-    sys.path = sys.path[1:]
+    long_description = open(
+        os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            'README.rst')).read()
+except:
+    long_description = 'Please refer to https://pytenable.readthedocs.io'
+    print('! could not read README.rst file.')
 
-
-setup(name="lorax",
-      version=vernum,
-      description="Lorax",
-      long_description="Tools for creating bootable images, including the Anaconda boot.iso",
-      author="Martin Gracik, Will Woods <wwoods@redhat.com>, Brian C. Lane <bcl@redhat.com>",
-      author_email="bcl@redhat.com",
-      url="http://www.github.com/weldr/lorax/",
-      download_url="http://www.github.com/weldr/lorax/releases/",
-      license="GPLv2+",
-      packages=["pylorax", "composer", "composer.cli"],
-      package_dir={"" : "src"},
-      data_files=data_files
-      )
+setup(
+    name='pyTenable',
+    version=version,
+    description='Python library to interface into Tenable\'s products and applications',
+    author='Tenable, Inc.',
+    long_description=long_description,
+    author_email='smcgrath@tenable.com',
+    url='https://github.com/tenable/pytenable',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='tenable tenable_io securitycenter containersecurity',
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=[
+        'requests>=2.19',
+        'python-dateutil>=2.6',
+        'semver>=2.8.1',
+        'ipaddress>=1.0.22',
+        'restfly>=1.3.5',
+        'marshmallow>=3.6',
+        'python-box>=4.0',
+    ],
+    extras_require={
+        'NessusReportv2': ['defusedxml>=0.5.0'],
+        'PWCertAuth': ['requests-pkcs12>=1.3'],
+        'docker': ['docker>=3.7.2'],
+        'complete': [
+            'defusedxml>=0.5.0',
+            'requests-pkcs12>=1.3',
+            'docker>=3.7.2',
+        ]
+    }
+)
--- tests/lorax/lib/lib.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# lorax specific functions
-
-. "$(dirname $0)/../cli/lib/lib.sh"
