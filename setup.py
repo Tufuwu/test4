aea61f24c8e0ab752a52edf857233234b250646e
wu <wu@.com>
2025-04-07 02:46:26 +0800

    rm-hull/luma.emulator/action

--- setup.py
+++ setup.py
@@ -1,82 +1,94 @@
-# Copyright 2019-2020 Canonical Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-"""Setup script for the Operator Framework."""
-
-from importlib.util import spec_from_file_location, module_from_spec
-from pathlib import Path
+import re
+import os
+import sys
+from io import open
 from setuptools import setup, find_packages
 
 
-def _read_me() -> str:
-    """Return the README content from the file."""
-    with open("README.md", "rt", encoding="utf8") as fh:
-        readme = fh.read()
-    return readme
+def read_file(fname, encoding='utf-8'):
+    with open(os.path.join(os.path.dirname(__file__), fname), encoding=encoding) as r:
+        return r.read()
 
 
-def _get_version() -> str:
-    """Get the version via ops/version.py, without loading ops/__init__.py."""
-    spec = spec_from_file_location('ops.version', 'ops/version.py')
-    module = module_from_spec(spec)
-    spec.loader.exec_module(module)
+def find_version(*file_paths):
+    fpath = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = read_file(fpath)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
 
-    return module.version
+    err_msg = 'Unable to find version string in {}'.format(fpath)
+    raise RuntimeError(err_msg)
 
 
-version = _get_version()
-version_path = Path("ops/version.py")
-version_backup = Path("ops/version.py~")
-if version_backup.exists():
-    # unlink(missing_ok=True) is a 3.8-ish
-    version_backup.unlink()
-version_path.rename(version_backup)
-try:
-    with version_path.open("wt", encoding="utf8") as fh:
-        fh.write('''\
-# this is a generated file
+README = read_file('README.rst')
+CONTRIB = read_file('CONTRIBUTING.rst')
+CHANGES = read_file('CHANGES.rst')
+version = find_version('luma', 'emulator', '__init__.py')
 
-version = {!r}
-'''.format(version))
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+test_deps = [
+    'pytest',
+    'pytest-cov',
+    'pytest-timeout'
+]
 
-    setup(
-        name="ops",
-        version=version,
-        description="The Python library behind great charms",
-        long_description=_read_me(),
-        long_description_content_type="text/markdown",
-        license="Apache-2.0",
-        url="https://github.com/canonical/operator",
-        author="The Charmcraft team at Canonical Ltd.",
-        author_email="charmcraft@lists.launchpad.net",
-        packages=find_packages(include=('ops', 'ops.*')),
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: Apache Software License",
-            "Development Status :: 4 - Beta",
-
-            "Intended Audience :: Developers",
-            "Intended Audience :: System Administrators",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: POSIX :: Linux",
-            # include Windows once we're running tests there also
-            # "Operating System :: Microsoft :: Windows",
+setup(
+    name="luma.emulator",
+    version=version,
+    author="Richard Hull",
+    author_email="richard.hull@destructuring-bind.org",
+    description=("A suite of pseudo-devices for luma.core components"),
+    long_description="\n\n".join([README, CONTRIB, CHANGES]),
+    long_description_content_type="text/x-rst",
+    license="MIT",
+    keywords="raspberry orange banana pi rpi opi sbc oled lcd led display screen spi i2c emulator",
+    url="https://github.com/rm-hull/luma.emulator",
+    download_url="https://github.com/rm-hull/luma.emulator/tarball/" + version,
+    namespace_packages=["luma"],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"luma.emulator.images": [
+        "luma/emulator/images/led_on.png",
+        "luma/emulator/images/led_off.png",
+        "luma/emulator/images/7-segment.png"
+    ]},
+    install_requires=[
+        "luma.core>=1.14.0",
+        "pygame"
+    ],
+    setup_requires=pytest_runner,
+    tests_require=test_deps,
+    extras_require={
+        'docs': [
+            'sphinx >= 1.5.1'
         ],
-        python_requires='>=3.5',
-        install_requires=["PyYAML"],
-    )
-
-finally:
-    version_path.unlink()
-    version_backup.rename(version_path)
+        'qa': [
+            'rstcheck',
+            'flake8'
+        ],
+        'test': test_deps
+    },
+    zip_safe=False,
+    python_requires='>=3.6, <4',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "Topic :: Education",
+        "Topic :: System :: Hardware",
+        "Topic :: System :: Hardware :: Hardware Drivers",
+        "Topic :: Software Development :: Libraries :: pygame",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ]
+)
