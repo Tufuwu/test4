3c5698647c6b2a63e133c78fdd54303f881837ec
wu <wu@.com>
2025-04-07 01:49:37 +0800

    burnysc2/python-sc2/action

--- setup.py
+++ setup.py
@@ -1,51 +1,36 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
 
-import os
-import sys
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
 
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist upload")
-    sys.exit()
-
-readme = open("README.rst").read()
-history = open("HISTORY.rst").read().replace(".. :changelog:", "")
-import djangorestframework_camel_case
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile["packages"], r=False)
+test_requirements = convert_deps_to_pip(pfile["dev-packages"], r=False)
 
 setup(
-    name="djangorestframework-camel-case",
-    version=djangorestframework_camel_case.__version__,
-    description="Camel case JSON support for Django REST framework.",
-    long_description=readme + "\n\n" + history,
-    long_description_content_type="text/x-rst",
-    author="Vitaly Babiy",
-    author_email="vbabiy86@gmail.com",
-    url="https://github.com/vbabiy/djangorestframework-camel-case",
-    packages=["djangorestframework_camel_case"],
-    package_dir={"djangorestframework_camel_case": "djangorestframework_camel_case"},
-    include_package_data=True,
-    python_requires=">=3.5",
-    install_requires=[],
-    license="BSD",
-    zip_safe=False,
-    keywords="djangorestframework_camel_case",
+    name="burnysc2",
+    packages=find_packages(exclude=["examples*", "examples"]),
+    version="4.11.2",
+    description="A StarCraft II API Client for Python 3",
+    license="MIT",
+    author="BurnySc2",
+    author_email="gamingburny@gmail.com",
+    url="https://github.com/Burnysc2/python-sc2",
+    keywords=["StarCraft", "StarCraft 2", "StarCraft II", "AI", "Bot"],
+    setup_requires=["pipenv"],
+    install_requires=requirements,
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Topic :: Games/Entertainment",
+        "Topic :: Games/Entertainment :: Real Time Strategy",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
     ],
-    test_suite="tests",
 )
