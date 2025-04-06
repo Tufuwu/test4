cbbe8d1dd6142392e5eed8aae28ce2617ec53beb
wu <wu@.com>
2025-04-06 21:22:20 +0800

    exxamalte/python-georss-ingv-centro-nazionale-terremoti-client/action

--- setup.py
+++ setup.py
@@ -1,40 +1,46 @@
-from setuptools import setup, find_packages
+import os
+
+from setuptools import find_packages, setup
+
+NAME = "georss_ingv_centro_nazionale_terremoti_client"
+AUTHOR = "Malte Franken"
+AUTHOR_EMAIL = "coding@subspace.de"
+DESCRIPTION = "A GeoRSS client library for the INGV Centro Nazionale Terremoti (Earthquakes) feed."
+URL = (
+    "https://github.com/exxamalte/python-georss-ingv-centro-nazionale-terremoti-client"
+)
+
+REQUIRES = [
+    "georss_client>=0.14",
+]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+VERSION = {}
+with open(os.path.join(HERE, NAME, "__version__.py")) as f:
+    exec(f.read(), VERSION)  # pylint: disable=exec-used
 
 setup(
-    name="django-statici18n",
-    version="2.1.1",
-    author="Sebastien Fievet",
-    author_email="zyegfryed@gmail.com",
-    url="http://django-statici18n.readthedocs.org/",
-    description="A Django app that provides helper for generating "
-    "Javascript catalog to static files.",
-    long_description=open("README.rst").read(),
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        "Django>=2.2",
-        "django-appconf>=1.0",
-    ],
-    license="BSD",
+    name=NAME,
+    version=VERSION["__version__"],
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    license="Apache-2.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=URL,
+    packages=find_packages(exclude=("tests*",)),
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
     ],
-    project_urls={
-        "Source": "https://github.com/zyegfryed/django-statici18n",
-    },
+    install_requires=REQUIRES,
 )
