f28106e9e61e0240ab75138a4190c060c6f5628b
wu <wu@.com>
2025-04-07 02:06:08 +0800

    pyudev/pyudev/action

--- setup.py
+++ setup.py
@@ -1,64 +1,66 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+# Copyright (C) 2010, 2011, 2012 Sebastian Wiesner <lunaryorn@gmail.com>
 
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 2.1 of the License, or (at your
+# option) any later version.
+
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+# isort: STDLIB
+import os
+import sys
+
+# isort: THIRDPARTY
 import setuptools
-import versioneer
 
-short_description = "Optimizing numpys einsum function"
-try:
-    with open("README.md", "r") as handle:
-        long_description = handle.read()
-except:
-    long_description = short_description
+if sys.version_info[0] < 3:
+    from codecs import open
 
 
-if __name__ == "__main__":
-    setuptools.setup(
-        name='opt_einsum',
-        version=versioneer.get_version(),
-        cmdclass=versioneer.get_cmdclass(),
-        description=short_description,
-        author='Daniel Smith',
-        author_email='dgasmith@icloud.com',
-        url="https://github.com/dgasmith/opt_einsum",
-        license='MIT',
-        packages=setuptools.find_packages(),
-        python_requires='>=3.5',
-        install_requires=[
-            'numpy>=1.7',
-        ],
-        extras_require={
-            'docs': [
-                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
-                'sphinxcontrib-napoleon',
-                'sphinx_rtd_theme',
-                'numpydoc',
-            ],
-            'tests': [
-                'pytest',
-                'pytest-cov',
-                'pytest-pep8',
-            ],
-        },
+def local_file(name):
+    return os.path.relpath(os.path.join(os.path.dirname(__file__), name))
 
-        tests_require=[
-            'pytest',
-            'pytest-cov',
-            'pytest-pep8',
-        ],
 
-        classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Science/Research',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-        ],
-        zip_safe=True,
-        long_description=long_description,
-        long_description_content_type="text/markdown"
-    )
+README = local_file("README.rst")
+
+with open(local_file("src/pyudev/version.py")) as o:
+    exec(o.read())
+
+setuptools.setup(
+    name="pyudev",
+    version=__version__,
+    url="http://pyudev.readthedocs.org/",
+    author="Sebastian Wiesner",
+    author_email="lunaryorn@gmail.com",
+    description="A libudev binding",
+    long_description=open(README, encoding="utf-8").read(),
+    platforms=["Linux"],
+    license="LGPL 2.1+",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Hardware",
+        "Topic :: System :: Operating System Kernels :: Linux",
+    ],
+    install_requires=["six"],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages("src"),
+)
