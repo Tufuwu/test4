5d444ae04207ce429d10f97cd2b1be4906634b83
wu <wu@.com>
2025-04-07 01:27:26 +0800

    circus-tent/circus/action

--- setup.py
+++ setup.py
@@ -1,33 +1,61 @@
-from setuptools import setup
+import sys
+from setuptools import setup, find_packages
+from circus import __version__
 
-setup(
-    name='rnc2rng',
-    version='2.6.1',
-    url='https://github.com/djc/rnc2rng',
-    author='David Mertz',
-    description='RELAX NG Compact to regular syntax conversion library',
-    maintainer='Dirkjan Ochtman',
-    maintainer_email='dirkjan@ochtman.nl',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'License :: Public Domain',
-        'Topic :: Text Processing :: Markup :: XML',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-    packages=['rnc2rng'],
-    entry_points={
-        'console_scripts': [
-            'rnc2rng = rnc2rng.__main__:main',
+if not hasattr(sys, 'version_info') or sys.version_info < (3, 5, 0, 'final'):
+    raise SystemExit("Circus requires Python 3.5 or higher.")
+
+
+install_requires = ['psutil', 'pyzmq>=17.0', 'tornado>=5.0.2']
+
+try:
+    import argparse     # NOQA
+except ImportError:
+    install_requires.append('argparse')
+
+with open("README.md") as f:
+    README = f.read()
+
+
+setup(name='circus',
+      version=__version__,
+      packages=find_packages(exclude=["docs", "examples"]),
+      description=("Circus is a program that will let you run and watch "
+                   " multiple processes and sockets."),
+      long_description=README,
+      author="Mozilla Foundation & contributors",
+      author_email="services-dev@lists.mozila.org",
+      include_package_data=True,
+      zip_safe=False,
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "License :: OSI Approved :: Apache Software License"
+      ],
+      install_requires=install_requires,
+      extras_require={
+        'test': [
+            'nose',
+            'nose-cov',
+            'coverage',
+            'mock',
+            'circus-web',
+            'gevent',
+            'papa',
+            'PyYAML',
+            'pyzmq>=17.0',
+            'flake8==2.1.0',
         ],
-    },
-    use_2to3=True,
-    install_requires=['rply'],
-)
+      },
+      test_suite='circus.tests',
+      entry_points="""
+      [console_scripts]
+      circusd = circus.circusd:main
+      circusd-stats = circus.stats:main
+      circusctl = circus.circusctl:main
+      circus-top = circus.stats.client:main
+      circus-plugin = circus.plugins:main
+      """)
