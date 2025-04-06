b703d315333489cdf00b825fc17ba76da15dd439
wu <wu@.com>
2025-04-07 00:45:47 +0800

    django/django-localflavor/action

--- setup.py
+++ setup.py
@@ -1,91 +1,147 @@
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
-
 import os
+import re
 import sys
-from setuptools import setup
+import codecs
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
 
-version_ns = {}
-with open(os.path.join(here, 'kernel_gateway', '_version.py')) as f:
-    exec(f.read(), {}, version_ns)
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
 
-setup_args = dict(
-    name='jupyter_kernel_gateway',
-    author='Jupyter Development Team',
-    author_email='jupyter@googlegroups.com',
-    url='http://github.com/jupyter-incubator/kernel_gateway',
-    description='A web server for spawning and communicating with Jupyter kernels',
-    long_description='''\
-Jupyter Kernel Gateway is a web server that supports different mechanisms for
-spawning and communicating with Jupyter kernels, such as:
 
-* A Jupyter Notebook server-compatible HTTP API used for requesting kernels
-  and talking the `Jupyter kernel protocol <https://jupyter-client.readthedocs.io/en/latest/messaging.html>`_
-  with the kernels over Websockets
-* A HTTP API defined by annotated notebook cells that maps HTTP verbs and
-  resources to code to execute on a kernel
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
-The server launches kernels in its local process/filesystem space. It can be
-containerized and scaled out using common technologies like
-`tmpnb <https://github.com/jupyter/tmpnb>`_,
-`Cloud Foundry <https://github.com/cloudfoundry>`_, and
-`Kubernetes <http://kubernetes.io/>`_.
-''',
-    version=version_ns['__version__'],
+
+# Provided as an attribute, so you can append to these instead
+# of replicating them:
+standard_exclude = ('*.py', '*.pyc', '*$py.class', '*~', '.*', '*.bak')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build',
+                                './dist', 'EGG-INFO', '*.egg-info')
+
+
+# (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
+# Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+# Note: you may want to copy this into your setup.py file verbatim, as
+# you can't import this from another package, when you don't know if
+# that package is installed yet.
+def find_package_data(where='.', package='',
+                      exclude=standard_exclude,
+                      exclude_directories=standard_exclude_directories,
+                      only_in_packages=True,
+                      show_ignored=False):
+    """
+    Return a dictionary suitable for use in ``package_data``
+    in a distutils ``setup.py`` file.
+
+    The dictionary looks like::
+
+        {'package': [files]}
+
+    Where ``files`` is a list of all the files in that package that
+    don't match anything in ``exclude``.
+
+    If ``only_in_packages`` is true, then top-level directories that
+    are not packages won't be included (but directories under packages
+    will).
+
+    Directories matching any pattern in ``exclude_directories`` will
+    be ignored; by default directories with leading ``.``, ``CVS``,
+    and ``_darcs`` will be ignored.
+
+    If ``show_ignored`` is true, then all the files that aren't
+    included in package data are shown on stderr (for debugging
+    purposes).
+
+    Note patterns use wildcards, or can be exact paths (including
+    leading ``./``), and all searching is case-insensitive.
+    """
+
+    out = {}
+    stack = [(convert_path(where), '', package, only_in_packages)]
+    while stack:
+        where, prefix, package, only_in_packages = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern) or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print >> sys.stderr, (
+                                "Directory %s ignored by pattern %s"
+                                % (fn, pattern))
+                        break
+                if bad_name:
+                    continue
+                if (os.path.isfile(os.path.join(fn, '__init__.py')) and not prefix):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                    stack.append((fn, '', new_package, False))
+                else:
+                    stack.append((fn, prefix + name + '/', package, only_in_packages))
+            elif package or not only_in_packages:
+                # is a file
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern) or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print >> sys.stderr, (
+                                "File %s ignored by pattern %s"
+                                % (fn, pattern))
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix + name)
+    return out
+
+setup(
+    name="django-localflavor",
+    version=find_version("localflavor", "__init__.py"),
+    url='https://django-localflavor.readthedocs.io/en/latest/',
     license='BSD',
-    platforms="Linux, Mac OS X, Windows",
-    keywords=['Interactive', 'Interpreter', 'Kernel', 'Web', 'Cloud'],
-    packages=[
-        'kernel_gateway',
-        'kernel_gateway.base',
-        'kernel_gateway.jupyter_websocket',
-        'kernel_gateway.notebook_http',
-        'kernel_gateway.notebook_http.cell',
-        'kernel_gateway.notebook_http.swagger',
-        'kernel_gateway.services',
-        'kernel_gateway.services.kernels',
-        'kernel_gateway.services.kernelspecs',
-        'kernel_gateway.services.sessions',
-    ],
-    scripts=[
-        'scripts/jupyter-kernelgateway'
-    ],
+    description="Country-specific Django helpers",
+    long_description=read('README.rst'),
+    author='Django Software Foundation',
+    author_email='foundation@djangoproject.com',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    package_data=find_package_data(),
     install_requires=[
-        'jupyter_core>=4.4.0',
-        'jupyter_client>=5.2.0',
-        'notebook>=5.7.6,<7.0',
-        'traitlets>=4.2.0',
-        'tornado>=4.2.0',
-        'requests>=2.7,<3.0'
+        'django>=2.2',
+        'python-stdnum>=1.6',
     ],
-    python_requires='>=3.6.1',
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
         'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
     ],
-    include_package_data=True,
+    zip_safe=False,
 )
-
-if 'setuptools' in sys.modules:
-    # setupstools turns entrypoint scripts into executables on windows
-    setup_args['entry_points'] = {
-        'console_scripts': [
-            'jupyter-kernelgateway = kernel_gateway:launch_instance'
-        ]
-    }
-    # Don't bother installing the .py scripts if if we're using entrypoints
-    setup_args.pop('scripts', None)
-
-if __name__ == '__main__':
-    setup(**setup_args)
