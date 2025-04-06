c6e88a87c20191a3db9f338002c9f6d73930feed
wu <wu@.com>
2025-04-06 21:25:52 +0800

    ebook-utils/css-parser/action

--- README.md
+++ README.md
@@ -1,74 +1,17 @@
-# BayesO Benchmarks
-[![Build Status](https://app.travis-ci.com/jungtaekkim/bayeso-benchmarks.svg?branch=main)](https://app.travis-ci.com/jungtaekkim/bayeso-benchmarks)
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+css-parser
+================
 
-Benchmarks for Bayesian optimization.
-The details of benchmark functions can be found in [these notes](http://jungtaek.github.io/notes/benchmarks_bo.pdf).
+[![Build Status](https://github.com/kovidgoyal/kitty/workflows/CI/badge.svg)](https://github.com/kovidgoyal/kitty/actions?query=workflow%3ACI)
 
-## Installation
-We recommend installing it with `virtualenv`.
-You can choose one of three installation options.
+A fork of the cssutils project based on version 1.0.2.
+This fork includes general bug fixes and extensions 
+specific to editing and working with ebooks.
 
-* Using PyPI repository (for user installation)
+The main python source code has been modified so
+that it will run without further conversion on both 
+Python >= 2.7 and Python 3.X without any further
+modules required.  All required modifications
+are handled local to each file
 
-To install the released version in PyPI repository, command it.
-
-```shell
-$ pip install bayeso-benchmarks
-```
-
-* Using source code (for developer installation)
-
-To install `bayeso-benchmarks` from source code, command
-
-```shell
-$ pip install .
-```
-in the `bayeso-benchmarks` root.
-
-* Using source code (for editable development mode)
-
-To use editable development mode, command
-
-```shell
-$ pip install -r requirements.txt
-$ python setup.py develop
-```
-in the `bayeso-benchmarks` root.
-
-* Uninstallation
-
-If you would like to uninstall `bayeso-benchmarks`, command it.
-
-```shell
-$ pip uninstall bayeso-benchmarks
-```
-
-## Required Packages
-Mandatory pacakges are inlcuded in `requirements.txt`.
-The following `requirements` files include the package list, the purpose of which is described as follows.
-
-* `requirements-dev.txt`: It is for developing the `bayeso-benchmarks` package.
-
-## Simple Example
-A simple example on Branin function is shown below.
-```python
-from bayeso_benchmarks import Branin
-
-obj_fun = Branin()
-bounds = obj_fun.get_bounds()
-
-X = obj_fun.sample_uniform(100)
-
-Y = obj_fun.output(X)
-Y_noise = obj_fun.output_gaussian_noise(X)
-```
-
-## Author
-* [Jungtaek Kim](http://jungtaek.github.io) (POSTECH)
-
-## Contact
-* Jungtaek Kim: [jtkim@postech.ac.kr](mailto:jtkim@postech.ac.kr)
-
-## License
-[MIT License](LICENSE)
+For more information on usage, please see the cssutils 
+documentation
--- setup.py
+++ setup.py
@@ -1,25 +1,72 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# License: LGPLv3 Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
 
-path_requirements = 'requirements.txt'
-list_packages = ['bayeso_benchmarks']
+import ast
+import re
+import sys
+import os
 
-with open(path_requirements) as f:
-    required = f.read().splitlines()
+from setuptools import find_packages, setup
+from setuptools.command.test import test
+
+# extract the version without importing the module
+VERSION = open('src/css_parser/version.py', 'rb').read().decode('utf-8')
+VERSION = '.'.join(map(str, ast.literal_eval(re.search(r'^version\s+=\s+(.+)', VERSION, flags=re.M).group(1))))
+long_description = '\n' + open('README.md', 'rb').read().decode('utf-8') + '\n'  # + read('CHANGELOG.txt')
+
+
+class Test(test):
+
+    user_options = [
+        ('which-test=', 'w', "Specify which test to run as either"
+            " the test method name (without the leading test_)"
+            " or a module name with a trailing period"),
+    ]
+
+    def initialize_options(self):
+        self.which_test = None
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import importlib
+        orig = sys.path[:]
+        try:
+            sys.path.insert(0, os.getcwd())
+            m = importlib.import_module('run_tests')
+            which_test = (self.which_test,) if self.which_test else ()
+            m.run_tests(which_test)
+        finally:
+            sys.path = orig
+
 
 setup(
-    name='bayeso-benchmarks',
-    version='0.1.6',
-    author='Jungtaek Kim',
-    author_email='jtkim@postech.ac.kr',
-    url='https://github.com/jungtaekkim/bayeso-benchmarks',
-    license='MIT',
-    description='Benchmarks for Bayesian optimization',
-    packages=list_packages,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, != 3.3.*, !=3.4.*, !=3.5.*, <4',
-    install_requires=required,
+    name='css-parser',
+    version=VERSION,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    description='A CSS Cascading Style Sheets library for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    cmdclass={'test': Test},
+    author='Various People',
+    author_email='redacted@anonymous.net',
+    url='https://github.com/ebook-utils/css-parser',
+    license='LGPL 3.0 or later',
+    keywords='CSS, Cascading Style Sheets, CSSParser, DOM Level 2 Stylesheets, DOM Level 2 CSS',
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: HTML'
     ]
 )
