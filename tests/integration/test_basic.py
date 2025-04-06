0826216e54f120c85cdb8cd0034ded4923827508
wu <wu@.com>
2025-04-07 02:56:59 +0800

    networktocode/ntc-templates/action

--- tests/integration/test_basic.py
+++
@@ -1,9 +0,0 @@
-import neo4j
-
-from tests.integration import settings
-
-
-def test_neo4j_connection():
-    driver = neo4j.GraphDatabase.driver(settings.get("NEO4J_URL"))
-    with driver.session() as session:
-        session.run("CALL db.schema();")
