6e1f4317d74bfde789cdcdf2bfffbc6d83c35277
wu <wu@.com>
2025-04-07 00:13:45 +0800

    pyxem/orix/action

--- setup.py
+++ setup.py
@@ -1,52 +1,50 @@
-import os
+from itertools import chain
+from setuptools import setup, find_packages
+from orix import __name__, __version__, __author__, __author_email__, __description__
 
-import setuptools
+# Projects with optional features for building the documentation and running
+# tests. From setuptools:
+# https://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-extras-optional-features-with-their-own-dependencies
+extra_feature_requirements = {
+    "doc": ["sphinx >= 3.0.2", "sphinx-rtd-theme >= 0.4.3"],
+    "tests": ["pytest >= 5.4", "pytest-cov >= 2.8.1", "coverage >= 5.0"],
+}
+extra_feature_requirements["dev"] = ["black >= 19.3b0", "pre-commit >= 1.16"] + list(
+    chain(*list(extra_feature_requirements.values()))
+)
 
-
-def local_file(name: str) -> str:
-    """Interpret filename as relative to this file."""
-    return os.path.relpath(os.path.join(os.path.dirname(__file__), name))
-
-
-SOURCE = local_file("src")
-README = local_file("README.md")
-
-with open(local_file("src/hypothesis_jsonschema/__init__.py")) as o:
-    for line in o:
-        if line.startswith("__version__"):
-            _, __version__, _ = line.split('"')
-
-
-setuptools.setup(
-    name="hypothesis-jsonschema",
-    version=__version__,
-    author="Zac Hatfield-Dodds",
-    author_email="zac@hypothesis.works",
-    packages=setuptools.find_packages(SOURCE),
-    package_dir={"": SOURCE},
-    package_data={"": ["py.typed"]},
-    url="https://github.com/Zac-HD/hypothesis-jsonschema",
-    project_urls={"Funding": "https://github.com/sponsors/Zac-HD"},
-    license="MPL 2.0",
-    description="Generate test data from JSON schemata with Hypothesis",
-    zip_safe=False,
-    install_requires=["hypothesis>=5.3.0", "jsonschema>=3.0.2"],
-    python_requires=">=3.6",
+setup(
+    name=__name__,
+    version=str(__version__),
+    license="GPLv3",
+    author=__author__,
+    author_email=__author_email__,
+    description=__description__,
+    long_description=open("README.rst").read(),
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Framework :: Hypothesis",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Topic :: Education :: Testing",
-        "Topic :: Software Development :: Testing",
-        "Typing :: Typed",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
     ],
-    long_description=open(README).read(),
-    long_description_content_type="text/markdown",
-    keywords="python testing fuzzing property-based-testing json-schema",
+    packages=find_packages(exclude=["orix/tests"]),
+    extras_require=extra_feature_requirements,
+    # fmt: off
+    install_requires=[
+        "diffpy.structure >= 3",
+        "h5py",
+        "matplotlib >= 3.3",
+        "numpy",
+        "scipy",
+        "tqdm",
+    ],
+    # fmt: on
+    package_data={"": ["LICENSE", "README.rst", "readthedocs.yml"], "orix": ["*.py"],},
 )
