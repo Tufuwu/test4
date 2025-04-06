b605cde9bd6ff30fef7be11e967cdf9befe17d75
wu <wu@.com>
2025-04-07 00:36:51 +0800

    peshay/btcde/action

--- setup.py
+++ setup.py
@@ -1,27 +1,23 @@
-from setuptools import setup
+#!/usr/bin/env python
+"""Setup to install Bitcoin.de API Python Module."""
+from distutils.core import setup
+setup(name='btcde',
+      version='4.0',
+      py_modules=['btcde'],
+      install_requires=['requests', 'future'],
+      description='API Wrapper for Bitcoin.de Trading API.',
+      url='https://github.com/peshay/btcde',
+      author='Andreas Hubert',
+      author_email='anhubert@gmail.com',
+      license='MIT',
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Developers',
+                   'Topic :: Software Development :: Libraries :: Application '
+                   'Frameworks',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9', ],
+      keywords='bitcoin.de bitcoin btc api',
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-requires = []
-with open('requirements.txt') as f:
-    for line in f.readlines():
-        line = line.strip()  # Remove spaces
-        line = line.split('#')[0]  # Remove comments
-        if line:  # Remove empty lines
-            requires.append(line)
-
-setup(
-    name='django-clickhouse',
-    version='1.0.3',
-    packages=['django_clickhouse'],
-    package_dir={'': 'src'},
-    url='https://github.com/carrotquest/django-clickhouse',
-    license='BSD 3-clause "New" or "Revised" License',
-    author='Carrot quest',
-    author_email='m1ha@carrotquest.io',
-    description='Django extension to integrate with ClickHouse database',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    install_requires=requires
-)
+      )
